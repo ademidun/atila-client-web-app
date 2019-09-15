db4abed43d3a835694b0633cc17169fafd896b50
ademidun <tomiademidun@gmail.com>
2019-09-15 15:40:29 -0400

    added ScholarshipAddEdit components

--- src/scenes/Scholarship/ScholarshipAddEdit.js
+++ src/scenes/Scholarship/ScholarshipAddEdit.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import ScholarshipAddEditForm from "./ScholarshipAddEditForm";
 
-function ScholarshipAddEdit() {
-    return (
-        <div>
-            <h2>ScholarshipAddEdit</h2>
-        </div>
-    );
+class ScholarshipAddEdit extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            scholarship: {
+                name: '',
+                slug: '',
+                description: '',
+                img_url: '',
+                scholarship_url: '',
+                deadline: '',
+                funding_amount: '',
+                female_only: false,
+            }
+        }
+    }
+
+    updateForm = (event) => {
+        event.preventDefault();
+        const scholarship = this.state.scholarship;
+        scholarship[event.target.name] = event.target.value;
+        this.setState({scholarship});
+    };
+
+    render() {
+
+        const { scholarship } = this.state;
+        return (
+
+            <div className="container mt-5">
+                <div className="card shadow p-3">
+                <h2>Scholarship Add Edit: {scholarship.name}</h2>
+                <ScholarshipAddEditForm
+                    onUpdateForm={this.updateForm}
+                    scholarship={scholarship} />
+            </div>
+            </div>
+        );
+
+    }
 }
 
 export default ScholarshipAddEdit;
