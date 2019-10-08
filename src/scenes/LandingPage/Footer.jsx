77f49f02d02966f9fb97787a514eb436be77f17d
ademidun <tomiademidun@gmail.com>
2019-10-07 22:02:22 -0400

    updated footer links

--- src/scenes/LandingPage/Footer.jsx
+++ src/scenes/LandingPage/Footer.jsx
@@ -1,139 +1,131 @@
 import React from 'react';
-import {FormattedMessage, IntlProvider} from 'react-intl';
 import { Row, Col } from 'antd';
 import './Footer.scss';
-import cnLocale from "./zh-CN";
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram, faItunes,
+  faLinkedin,
+  faMedium,
+  faSpotify,
+  faTwitter,
+  faYoutube
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
-      <IntlProvider locale={cnLocale.locale} messages={cnLocale.messages}>
+      <React.Fragment>
         <hr />
         <footer id="footer">
           <div className="footer-wrap p-md-3 p-lg-5">
             <Row>
-              <Col md={6} sm={24} xs={24}>
+              <Col md={6} sm={12} xs={12}>
                 <div className="footer-center">
-                  <h2>Ant Design</h2>
                   <div>
-                    <a target="_blank " href="https://github.com/ant-design/ant-design">
-                      GitHub
+                    <Link to="/blog/atila/what-is-atila">
+                      About
+                    </Link>
+                  </div>
+                  <div>
+                    <Link to="/team">
+                      Team
+                    </Link>
+                  </div>
+                  <div>
+                    <Link to="/contact">
+                      Contact
+                    </Link>
+                  </div>
+                  <div>
+                    <Link to="/essay">
+                      Essays
+                    </Link>
+                  </div>
+                  <div>
+                    <Link to="/blog">
+                      Blogs
+                    </Link>
+                  </div>
+                </div>
+              </Col>
+              <Col md={6} sm={12} xs={12}>
+                <div className="footer-center">
+                  <div>
+                    <Link to="/profile/atilaeng">
+                      Engineering
+                    </Link>
+                  </div>
+                  <div>
+                    <a href="https://tech.atila.ca"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                    >
+                      Atila Tech
                     </a>
                   </div>
                   <div>
-                    <a href="http://pro.ant.design">Ant Design Pro</a>
+                    <Link to="/terms-conditions">
+                      Terms and Conditions
+                    </Link>
                   </div>
                   <div>
-                    <a href="http://mobile.ant.design">Ant Design Mobile</a>
+                    <Link to="/sitemap">
+                      SiteMap
+                    </Link>
                   </div>
                   <div>
-                    <a href="http://ng.ant.design">NG-ZORRO</a>
-                    <span> - </span>
-                    Ant Design of Angular
+                    <Link to="/blog">
+                      Blogs
+                    </Link>
                   </div>
+                </div>
+              </Col>
+              <Col md={6} sm={12} xs={12}>
+                <div className="footer-center">
                   <div>
-                    <a target="_blank " href="https://github.com/websemantics/awesome-ant-design">
-                      <FormattedMessage id="app.footer.awesome" />
+                    <a  target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCG2iWiYgJYkjBl4EdYGI5mw/">
+                      Youtube <FontAwesomeIcon icon={faYoutube} />
                     </a>
                   </div>
                   <div>
-                    <a target="_blank " href="http://ant-design.gitee.io/">
-                      <FormattedMessage id="app.footer.chinamirror" />
+                    <a  target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/show/0m74ZmCPgjvp5WGOMg3P9C">
+                      Spotify <FontAwesomeIcon icon={faSpotify} />
+                    </a>
+                  </div>
+                  <div>
+                    <a  target="_blank" rel="noopener noreferrer" href="https://itunes.apple.com/ca/podcast/id1440531021">
+                      Itunes <FontAwesomeIcon icon={faItunes} />
+                    </a>
+                  </div>
+                  <div>
+                    <a  target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/atilatech/">
+                      Instagram <FontAwesomeIcon icon={faInstagram} />
                     </a>
                   </div>
                 </div>
               </Col>
-              <Col md={6} sm={24} xs={24}>
+              <Col md={6} sm={12} xs={12}>
                 <div className="footer-center">
-                  <h2><FormattedMessage id="app.footer.resources" /></h2>
                   <div>
-                    <a href="http://scaffold.ant.design">Scaffolds</a>
-                    <span> - </span>
-                    <FormattedMessage id="app.footer.scaffolds" />
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva">dva</a> - <FormattedMessage id="app.footer.dva" />
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva-cli">dva-cli</a> -
-                    <FormattedMessage id="app.footer.dev-tools" />
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="http://motion.ant.design">Ant Motion</a>
-                    <span> - </span>
-                    <FormattedMessage id="app.footer.motion" />
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="http://library.ant.design/">AntD Library</a>
-                    <span> - </span>
-                    <FormattedMessage id="app.footer.antd-library" />
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="http://ux.ant.design">Ant UX</a>
-                    <span> - </span>
-                    <FormattedMessage id="app.footer.antux" />
-                  </div>
-                </div>
-              </Col>
-              <Col md={6} sm={24} xs={24}>
-                <div className="footer-center">
-                  <h2><FormattedMessage id="app.footer.community" /></h2>
-                  <div>
-                    <a href="/changelog">
-                      <FormattedMessage id="app.footer.change-log" />
+                    <a  target="_blank" rel="noopener noreferrer" href="https://www.divnkedin.com/company/atila-tech/">
+                      Linkedin <FontAwesomeIcon icon={faLinkedin} />
                     </a>
                   </div>
                   <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/wiki/FAQ">
-                      <FormattedMessage id="app.footer.faq" />
+                    <a  target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/atilatech1/">
+                      Facebook <FontAwesomeIcon icon={faFacebook} />
                     </a>
                   </div>
                   <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design">
-                      <FormattedMessage id="app.footer.discuss-cn" />
+                    <a  target="_blank" rel="noopener noreferrer" href="https://medium.com/atila-tech">
+                      Medium <FontAwesomeIcon icon={faMedium} />
                     </a>
                   </div>
                   <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design-english">
-                      <FormattedMessage id="app.footer.discuss-en" />
+                    <a  target="_blank" rel="noopener noreferrer" href="https://twitter.com/atilatech">
+                      Twitter <FontAwesomeIcon icon={faTwitter} />
                     </a>
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="http://new-issue.ant.design/">
-                      <FormattedMessage id="app.footer.bug-report" />
-                    </a>
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/issues">
-                      <FormattedMessage id="app.footer.issues" />
-                    </a>
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="http://stackoverflow.com/questions/tagged/antd">
-                      <FormattedMessage id="app.footer.stackoverflow" />
-                    </a>
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://segmentfault.com/t/antd">
-                      <FormattedMessage id="app.footer.segmentfault" />
-                    </a>
-                  </div>
-                </div>
-              </Col>
-              <Col md={6} sm={24} xs={24}>
-                <div className="footer-center">
-                  <h2>
-                    <img className="title-icon" src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg" alt="" />
-                    <FormattedMessage id="app.footer.more-product" />
-                  </h2>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://antv.alipay.com/">AntV</a>
-                    <span> - </span>
-                    <FormattedMessage id="app.footer.data-vis" />
-                  </div>
-                  <div>
-                    <a target="_blank" rel="noopener noreferrer" href="https://eggjs.org/">Egg</a>
-                    <span> - </span>
-                    <FormattedMessage id="app.footer.eggjs" />
                   </div>
                 </div>
               </Col>
@@ -155,7 +147,7 @@ function Footer() {
             </Col>
           </Row>
         </footer>
-      </IntlProvider>
+      </React.Fragment>
   );
 }
 
