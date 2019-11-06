396709a1e48d132d6a6b8ec777a8665dca4c134c
ademidun <tomiademidun@gmail.com>
2019-11-06 14:11:24 -0500

    refactored images in MoreFeatures

--- src/scenes/LandingPage/MoreFeatures.js
+++ src/scenes/LandingPage/MoreFeatures.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import scholarshipsNotificationsGif from './assets/scholarship-notifications.gif';
 import viewEssaysGif from './assets/view-essays.gif';
-
+import emailDigest1 from './assets/digest-email-screenshot-1.png';
+import emailDigest2 from './assets/digest-email-screenshot-2.png';
+import emailScholarshipDue from './assets/scholarship-due-email.png';
+import './MoreFeatures.scss';
 function MoreFeatures() {
 
     return (
-        <div  className="container">
+        <div  className="container MoreFeatures">
             <h1 className="col-sm-12 text-center">
                 <Link to="/blog/atila/what-is-atila"> More Features </Link>
             </h1>
@@ -28,8 +30,11 @@ function MoreFeatures() {
                         <p >Save scholarships you are interested in and we'll let you know 7 days and 24 hours before the scholarship is due.</p>
                     </div>
                     <div  className="card shadow m-3 p-3 col-lg-7 col-sm-12">
-                        <img  alt="Get Notified when Scholarships are Due" className="landing-page-gif landing-page-gif-mobile" id="scholarship-notifications-gif" src={scholarshipsNotificationsGif} title="Get Notified when Scholarships are Due" />
+                        <img  alt="Get Notified when Scholarships are Due"
+                              className="landing-page-gif landing-page-gif-mobile" id="scholarship-notifications-gif"
+                              src={emailScholarshipDue} title="Get Notified when Scholarships are Due" />
                     </div>
+
                 </div>
                 <div  className="row">
                     <div  className="card shadow m-3 p-5 col-lg-4 col-sm-12">
@@ -39,10 +44,16 @@ function MoreFeatures() {
                                 🕐
                             </span>
                         </h3>
-                        <p >Weekly scholarship reminders for scholarships just for you that are due soon</p>
+                        <p >Weekly scholarship reminders for scholarships customized just for you</p>
                     </div>
                     <div  className="card shadow m-3 p-3 col-lg-7 col-sm-12">
-                        <img  alt="Get Notified when Scholarships are Due" className="landing-page-gif landing-page-gif-mobile" id="scholarship-notifications-gif" src={scholarshipsNotificationsGif} title="Get Notified when Scholarships are Due" />
+                        <img  alt="Get Notified BEFORE Scholarships are Due"
+                              className="landing-page-gif landing-page-gif-mobile"
+                              src={emailDigest1} title="Get Notified when Scholarships are Due" />
+
+                        <img  alt="Get Notified BEFORE Scholarships are Due"
+                              className="landing-page-gif landing-page-gif-mobile"
+                              src={emailDigest2} title="Get Notified when Scholarships are Due" />
                     </div>
                 </div>
                 <div  className="row">
