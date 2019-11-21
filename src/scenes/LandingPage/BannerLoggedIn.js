c0740892f5229d92c715ee55326b5e78d044ac91
ademidun <tomiademidun@gmail.com>
2019-11-21 10:51:27 -0500

    fixed no-unused-vars in BannerLoggedIn

--- src/scenes/LandingPage/BannerLoggedIn.js
+++ src/scenes/LandingPage/BannerLoggedIn.js
@@ -7,7 +7,7 @@ import BannerImage from './BannerImage';
 import {Button} from "antd";
 import {Link} from "react-router-dom";
 import {enquireScreen} from 'enquire-js';
-import {getGreetingTime, slugify} from "../../services/utils";
+import {getGreetingTime} from "../../services/utils";
 import moneyFaceEmoji from './assets/moneyFaceEmoji.png';
 import {connect} from "react-redux";
 
