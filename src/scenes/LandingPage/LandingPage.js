79d2e6aeae1880e4535e5da0d892cf8199847b24
ademidun <tomiademidun@gmail.com>
2019-11-21 08:42:09 -0500

    removed enquire-js isMobile checking

--- src/scenes/LandingPage/LandingPage.js
+++ src/scenes/LandingPage/LandingPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {enquireScreen} from 'enquire-js';
 import Banner from './Banner';
 import 'antd/dist/antd.css';
 import './LandingPage.scss';
@@ -14,32 +13,14 @@ import {Link} from "react-router-dom";
 import HelmetSeo, {defaultSeoContent} from "../../components/HelmetSeo";
 import {blogs, essays} from "./LandingPageData";
 
-let isMobile = false;
-enquireScreen((b) => {
-    isMobile = b;
-});
-
 class LandingPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isMobile,
-        };
-    }
 
-    componentDidMount() {
-        enquireScreen((b) => {
-            this.setState({
-                isMobile: !!b,
-            });
-        });
-    }
     render() {
         return (
                 <div className="page-wrapper home">
                     <HelmetSeo content={defaultSeoContent}/>
-                    <Banner isMobile={this.state.isMobile} />
-                    <WhatIsAtila isMobile={this.state.isMobile} />
+                    <Banner/>
+                    <WhatIsAtila/>
                     <div className="p-5">
                         <Link to="/register" className="btn btn-primary center-block font-size-xl">
                             Register for Free
