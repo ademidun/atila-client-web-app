d3d361c05d83e05f593d87605c695718f4ce3525
ademidun <tomiademidun@gmail.com>
2019-11-30 22:22:41 -0500

    removed background floating things and blue bottom border in Landing page banner and fixed Banner height

--- src/scenes/LandingPage/Banner.jsx
+++ src/scenes/LandingPage/Banner.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TweenOne from 'rc-tween-one';
 import QueueAnim from 'rc-queue-anim';
-import ScrollParallax from 'rc-scroll-anim/lib/ScrollParallax';
 import {Button} from "antd";
 import {Link, withRouter} from "react-router-dom";
 import AutoComplete from "../../components/AutoComplete";
 import {MASTER_LIST_EVERYTHING_UNDERSCORE} from "../../models/ConstantsForm";
 import {slugify} from "../../services/utils";
 import moneyFaceEmoji from './assets/moneyFaceEmoji.png';
-
-const loop = {
-  duration: 3000,
-  yoyo: true,
-  repeat: -1,
-};
 
 class Banner extends React.Component {
 
@@ -60,19 +52,6 @@ class Banner extends React.Component {
     return (
       <div className="home-page-wrapper banner-wrapper" id="banner">
         <div className="banner-bg-wrapper">
-          <svg width="400px" height="576px" viewBox="0 0 400 576" fill="none">
-            <TweenOne component="g" animation={[{ opacity: 0, type: 'from' }, { ...loop, y: 15 }]}>
-              <ellipse id="Oval-9-Copy-4" cx="100" cy="100" rx="6" ry="6" stroke="#2F54EB" strokeWidth="1.6" />
-            </TweenOne>
-            <TweenOne component="g" animation={[{ opacity: 0, type: 'from' }, { ...loop, y: -15 }]}>
-              <g transform="translate(200 400)">
-                <g style={{ transformOrigin: '50% 50%', transform: 'rotate(-340deg)' }}>
-                  <rect stroke="#FADB14" strokeWidth="1.6" width="9" height="9" />
-                </g>
-              </g>
-            </TweenOne>
-          </svg>
-          <ScrollParallax location="banner" className="banner-bg" animation={{ playScale: [1, 1.5], rotate: 0 }} />
         </div>
         <QueueAnim className={`${className} page justify-content-center`} type="alpha" delay={150}>
           <QueueAnim
--- src/scenes/LandingPage/BannerLoggedIn.js
+++ src/scenes/LandingPage/BannerLoggedIn.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TweenOne from 'rc-tween-one';
 import QueueAnim from 'rc-queue-anim';
-import ScrollParallax from 'rc-scroll-anim/lib/ScrollParallax';
 import {Button} from "antd";
 import {Link} from "react-router-dom";
 import {getGreetingTime} from "../../services/utils";
 import moneyFaceEmoji from './assets/moneyFaceEmoji.png';
 import {connect} from "react-redux";
-
-const loop = {
-    duration: 3000,
-    yoyo: true,
-    repeat: -1,
-};
 
 class BannerLoggedIn extends React.Component {
 
@@ -31,21 +23,6 @@ class BannerLoggedIn extends React.Component {
         const  humanizedGreeting = `Good ${timeOfDay} ${first_name} ${greetingEmojiDict[timeOfDay]},`;
         return (
             <div className="home-page-wrapper logged-in" id="banner">
-                <div className="banner-bg-wrapper">
-                    <svg width="400px" height="576px" viewBox="0 0 400 576" fill="none">
-                        <TweenOne component="g" animation={[{ opacity: 0, type: 'from' }, { ...loop, y: 15 }]}>
-                            <ellipse id="Oval-9-Copy-4" cx="100" cy="100" rx="6" ry="6" stroke="#2F54EB" strokeWidth="1.6" />
-                        </TweenOne>
-                        <TweenOne component="g" animation={[{ opacity: 0, type: 'from' }, { ...loop, y: -15 }]}>
-                            <g transform="translate(200 400)">
-                                <g style={{ transformOrigin: '50% 50%', transform: 'rotate(-340deg)' }}>
-                                    <rect stroke="#FADB14" strokeWidth="1.6" width="9" height="9" />
-                                </g>
-                            </g>
-                        </TweenOne>
-                    </svg>
-                    <ScrollParallax location="banner" className="banner-bg" animation={{ playScale: [1, 1.5], rotate: 0 }} />
-                </div>
                 <QueueAnim className={`${className} page justify-content-center`}
                            type="alpha" delay={150}>
                     <QueueAnim
