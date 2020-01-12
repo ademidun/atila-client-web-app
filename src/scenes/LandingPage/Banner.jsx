fdda97ec37d9aebc2dfe5d6141302275ea9c2a33
ademidun <tomiademidun@gmail.com>
2020-01-12 11:27:48 -0500

    Revert "simplified LandingPage removed BannerImage"
    This reverts commit 25b4a8b2395280df8542a402cfac834a4f6129a3.

--- src/scenes/LandingPage/Banner.jsx
+++ src/scenes/LandingPage/Banner.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TweenOne from 'rc-tween-one';
 import QueueAnim from 'rc-queue-anim';
+import ScrollParallax from 'rc-scroll-anim/lib/ScrollParallax';
+import BannerImage from './BannerImage';
 import {Button} from "antd";
 import {Link, withRouter} from "react-router-dom";
 import AutoComplete from "../../components/AutoComplete";
 import {MASTER_LIST_EVERYTHING_UNDERSCORE} from "../../models/ConstantsForm";
 import {slugify} from "../../services/utils";
 import moneyFaceEmoji from './assets/moneyFaceEmoji.png';
+
+const loop = {
+  duration: 3000,
+  yoyo: true,
+  repeat: -1,
+};
 
 class Banner extends React.Component {
 
@@ -52,8 +61,21 @@ class Banner extends React.Component {
     return (
       <div className="home-page-wrapper banner-wrapper" id="banner">
         <div className="banner-bg-wrapper">
+          <svg width="400px" height="576px" viewBox="0 0 400 576" fill="none">
+            <TweenOne component="g" animation={[{ opacity: 0, type: 'from' }, { ...loop, y: 15 }]}>
+              <ellipse id="Oval-9-Copy-4" cx="100" cy="100" rx="6" ry="6" stroke="#2F54EB" strokeWidth="1.6" />
+            </TweenOne>
+            <TweenOne component="g" animation={[{ opacity: 0, type: 'from' }, { ...loop, y: -15 }]}>
+              <g transform="translate(200 400)">
+                <g style={{ transformOrigin: '50% 50%', transform: 'rotate(-340deg)' }}>
+                  <rect stroke="#FADB14" strokeWidth="1.6" width="9" height="9" />
+                </g>
+              </g>
+            </TweenOne>
+          </svg>
+          <ScrollParallax location="banner" className="banner-bg" animation={{ playScale: [1, 1.5], rotate: 0 }} />
         </div>
-        <QueueAnim className={`${className} page justify-content-center`} type="alpha" delay={150}>
+        <QueueAnim className={`${className} page`} type="alpha" delay={150}>
           <QueueAnim
             className="text-wrapper responsive-text"
             key="text"
@@ -109,6 +131,9 @@ class Banner extends React.Component {
               </div>
             </form>
           </QueueAnim>
+          <div className="img-wrapper" key="image">
+            <ScrollParallax location="banner" component={BannerImage} animation={{ playScale: [1, 1.5], y: 80 }} />
+          </div>
         </QueueAnim>
       </div>
     );
