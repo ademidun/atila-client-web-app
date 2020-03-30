97746c1f3050e0081823210cb6e5d0f3299964a4
ademidun <tomiademidun@gmail.com>
2020-03-30 19:22:50 -0400

    restyled schools and jobs ebook cover image

--- src/scenes/Ebook/EbookLandingBanner.js
+++ src/scenes/Ebook/EbookLandingBanner.js
@@ -7,13 +7,13 @@ function EbookLandingBanner() {
             <h1 className="col-sm-12 text-center">
                 Coming Soon
             </h1>
-            <div className="offset-lg-1">
-                <div className="row">
-                    <div className="card shadow m-3 p-5 col-lg-4 col-sm-12">
-                        <img src="https://i.imgur.com/EH9wKtQ.png" alt="Book cover"/>
-                    </div>
+
+            <div className="row">
+                <div className="card shadow col">
+                    <img src="https://i.imgur.com/EH9wKtQ.png" alt="Book cover"/>
+                </div>
             </div>
-        </div>
+
         </div>
     );
 }
