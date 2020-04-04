a26b34ea4380b819db88a2cdc153535cb85653e3
ademidun <tomiademidun@gmail.com>
2020-04-04 09:13:21 -0400

    fixed stylings for ebook banner page

--- src/scenes/Ebook/EbookLandingBanner.js
+++ src/scenes/Ebook/EbookLandingBanner.js
@@ -3,32 +3,30 @@ import React from "react";
 function EbookLandingBanner() {
   return (
     <div className='container'>
-      <h1 className='col-sm-12 text-center'>Coming Soon</h1>
+      <h1 className='col-sm-12 text-center'>Atila Schools and Jobs Report</h1>
 
       <div className='row'>
         <br />
-        <div className='  col' style={{ textAlign: "center" }}>
+        <div className="col text-center">
           <br />
-          <div style={{ paddingTop: "10%" }}>
-            <h4>Author: ATILA</h4>
-          </div>
           <div>
-            <h1 style={{ fontSize: "40px" }}>
-              We have an exciting book coming soon
-            </h1>
+            <h2 >
+                The best Canadian Universities for getting jobs at Goldman Sachs, Google, Mckinsey, Pfizer
+                and more.
+            </h2>
           </div>
-          <div style={{ textAlign: "center" }}>
-            <p>Featuring what the best universities are for jobs</p>
+          <div className="col text-center">
+            <p>Coming Soon</p>
           </div>
-          <div style={{ left: "50%", top: "50%" }}>
-            <button className='btn btn-primary' style={{ width: "100px" }}>
-              Buy Now for $9.99
+          <div className="col text-center">
+            <button className='btn btn-primary'>
+              Buy Now for $29.99
             </button>
           </div>
         </div>
 
         <div className='card shadow col'>
-          <div style={{ display: "table-cell", position: "relative" }}>
+          <div>
             <img
               src='https://i.imgur.com/EH9wKtQ.png'
               style={{ maxHeight: "100%", maxWidth: "100%" }}
