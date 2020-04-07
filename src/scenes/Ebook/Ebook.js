962a404b736ec5b72b0de7807beaae053e0aa10a
ademidun <tomiademidun@gmail.com>
2020-04-07 12:10:55 -0400

    update ebook SEO image

--- src/scenes/Ebook/Ebook.js
+++ src/scenes/Ebook/Ebook.js
@@ -10,7 +10,7 @@ function Ebook() {
       "Atila Schools and Jobs Report | The Best Canadian Universities for the Best Jobs",
     description:
       "The best Canadian Universities for getting jobs at Goldman Sachs, Google, McKinsey, Pfizer and more.",
-    image: "https://i.imgur.com/fejhJZT.png",
+    image: "https://i.imgur.com/cmpakw1.png",
     slug: "/schools",
   };
 
