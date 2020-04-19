bc1c271d8d8073ce2e6d1a50201aa29c1dd8720d
D P <dp@Ds-MacBook-Pro.local>
2020-04-19 02:45:04 -0400

    made tabs with each tab display a different embed

--- src/scenes/EbookPremium/EbookPremiumTabs.js
+++ src/scenes/EbookPremium/EbookPremiumTabs.js
@@ -1,77 +1,119 @@
 import React, { Component } from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Tabs } from "antd";
 import "../Ebook/Ebook.scss";
-import { Tab, Tabs } from "react-bootstrap";
+
+const TabPane = Tabs.TabPane;
+let viz;
 
 export default class EbookPremiumTabs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      industry: "InvestmentBanking",
+    };
+  }
+
   componentDidMount() {
-    const techTierVizUrl =
-      "https://public.tableau.com/views/EntryLevelGoogleSoftwareEngineerBaseSalaryinWaterloovsMountainViewCAD/Sheet5?:display_count=y&:origin=viz_share_link";
-    const biomedicalTierVizUrl =
-      "https://public.tableau.com/views/WhatSchoolsDoBiomedCompaniesHireFromOrganizedbyPosition/Sheet4?:display_count=y&:origin=viz_share_link";
-    const investmentbankingTierVizUrl =
+    console.log(this.state.industry);
+    const vizUrl =
       "https://public.tableau.com/views/EntryLevelGoogleProductManagervsGoldmanSachsAnalystinNYCUSD/Sheet4?:display_count=y&:origin=viz_share_link";
-    const consultingTierVizUrl =
-      "https://public.tableau.com/views/WhatSchoolsDoCompaniesHireFromOrganizedbyCompanySchoolsNumbers/Sheet2?:display_count=y&:origin=viz_share_link";
 
-    const techVizContainer = this.techVizContainer;
-    const biomedicalVizContainer = this.biomedicalVizContainer;
-    const investmentbankingVizContainer = this.investmentbankingVizContainer;
-    const consultingVizContainer = this.consultingVizContainer;
+    const vizContainer = this.vizContainer;
+    viz = new window.tableau.Viz(vizContainer, vizUrl);
+  }
 
-    /* eslint-disable no-unused-vars */
-    // noinspection JSUnusedLocalSymbols
-    let techTierViz = new window.tableau.Viz(techVizContainer, techTierVizUrl);
-    let biomedicalTierViz = new window.tableau.Viz(
-      biomedicalVizContainer,
-      biomedicalTierVizUrl
-    );
-    let investmentbankingTierViz = new window.tableau.Viz(
-      investmentbankingVizContainer,
-      investmentbankingTierVizUrl
-    );
-    let consultingTierViz = new window.tableau.Viz(
-      consultingVizContainer,
-      consultingTierVizUrl
-    );
+  componentDidUpdate() {
+    console.log(this.state.industry);
+
+    viz.dispose();
+
+    if (this.state.industry === "Tech") {
+      const vizUrl =
+        "https://public.tableau.com/views/EntryLevelGoogleSoftwareEngineerBaseSalaryinWaterloovsMountainViewCAD/Sheet5?:display_count=y&:origin=viz_share_link";
+      const vizContainer = this.vizContainer;
+      viz = new window.tableau.Viz(vizContainer, vizUrl);
+    }
+
+    if (this.state.industry === "Biomedical") {
+      const vizUrl =
+        "https://public.tableau.com/views/WhatSchoolsDoBiomedCompaniesHireFromOrganizedbyPosition/Sheet4?:display_count=y&:origin=viz_share_link";
+      const vizContainer = this.vizContainer;
+      viz = new window.tableau.Viz(vizContainer, vizUrl);
+    }
+    if (this.state.industry === "InvestmentBanking") {
+      const vizUrl =
+        "https://public.tableau.com/views/EntryLevelGoogleProductManagervsGoldmanSachsAnalystinNYCUSD/Sheet4?:display_count=y&:origin=viz_share_link";
+
+      const vizContainer = this.vizContainer;
+      viz = new window.tableau.Viz(vizContainer, vizUrl);
+    }
+
+    if (this.state.industry === "Consulting") {
+      const vizUrl =
+        "https://public.tableau.com/views/WhatSchoolsDoCompaniesHireFromOrganizedbyCompanySchoolsNumbers/Sheet2?:display_count=y&:origin=viz_share_link";
+      const vizContainer = this.vizContainer;
+      viz = new window.tableau.Viz(vizContainer, vizUrl);
+    }
   }
 
   render() {
+    const changeTab = (activeKey) => {
+      this.setState({
+        industry: activeKey,
+      });
+    };
+
     return (
       <Row>
         <Col>
           <div className='text-center'>
-            <Tabs transition={false} id='UserProfileViewTabs'>
-              <Tab eventKey='InvestmentBanking' title='Investment Banking'>
-                <div
-                  ref={(div) => {
-                    this.investmentbankingVizContainer = div;
-                  }}
-                ></div>
-              </Tab>
+            <Tabs
+              defaultActiveKey='InvestmentBanking'
+              id='UserProfileViewTabs'
+              onChange={(e) => changeTab(e)}
+            >
+              <TabPane tab='InvestmentBanking' key='InvestmentBanking'>
+                <React.Fragment>
+                  Hello
+                  <div
+                    ref={(div) => {
+                      this.vizContainer = div;
+                    }}
+                  ></div>
+                </React.Fragment>
+              </TabPane>
 
-              <Tab eventKey='Tech' title='Tech'>
-                <div
-                  ref={(div) => {
-                    this.techVizContainer = div;
-                  }}
-                ></div>
-              </Tab>
+              <TabPane tab='Tech' key='Tech'>
+                <React.Fragment>
+                  hi
+                  <div
+                    ref={(div) => {
+                      this.vizContainer = div;
+                    }}
+                  ></div>
+                </React.Fragment>
+              </TabPane>
 
-              <Tab eventKey='Biomedical' title='Biomedical'>
-                <div
-                  ref={(div) => {
-                    this.biomedicalVizContainer = div;
-                  }}
-                ></div>
-              </Tab>
-              <Tab eventKey='Consulting' title='Consulting'>
-                <div
-                  ref={(div) => {
-                    this.consultingVizContainer = div;
-                  }}
-                ></div>
-              </Tab>
+              <TabPane tab='Biomedical' key='Biomedical'>
+                <React.Fragment>
+                  howdy
+                  <div
+                    ref={(div) => {
+                      this.vizContainer = div;
+                    }}
+                  ></div>
+                </React.Fragment>
+              </TabPane>
+              <TabPane tab='Consulting' key='Consulting'>
+                <React.Fragment>
+                  meow
+                  <div
+                    ref={(div) => {
+                      this.vizContainer = div;
+                    }}
+                  ></div>
+                </React.Fragment>
+              </TabPane>
             </Tabs>
           </div>
         </Col>
