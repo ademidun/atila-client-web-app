a7f00cd265638a495972207dc9a768f185689ac7
ademidun <tomiademidun@gmail.com>
2020-04-26 11:40:57 -0400

    change industryConfig key name from vizUrl to url

--- src/scenes/EbookPremium/EbookPremiumTabs.js
+++ src/scenes/EbookPremium/EbookPremiumTabs.js
@@ -20,22 +20,22 @@ export default class EbookPremiumTabs extends Component {
         "Tech": {
           name: "Tech",
           label: "Google, Amazon, Facebook, and more",
-          vizUrl: "https://public.tableau.com/views/EntryLevelGoogleSoftwareEngineerBaseSalaryinWaterloovsMountainViewCAD/Sheet5?:display_count=y&:origin=viz_share_link",
+          url: "https://public.tableau.com/views/EntryLevelGoogleSoftwareEngineerBaseSalaryinWaterloovsMountainViewCAD/Sheet5?:display_count=y&:origin=viz_share_link",
         },
         "Biomedical": {
           name: "Biomedical",
           label: "Pfizer, GSK, and more",
-          vizUrl: "https://public.tableau.com/views/WhatSchoolsDoBiomedCompaniesHireFromOrganizedbyPosition/Sheet4?:display_count=y&:origin=viz_share_link",
+          url: "https://public.tableau.com/views/WhatSchoolsDoBiomedCompaniesHireFromOrganizedbyPosition/Sheet4?:display_count=y&:origin=viz_share_link",
         },
         "InvestmentBanking": {
           name: "InvestmentBanking",
           label: "Goldman Sachs, RBC Capital Markets and more",
-          vizUrl: "https://public.tableau.com/views/EntryLevelGoogleProductManagervsGoldmanSachsAnalystinNYCUSD/Sheet4?:display_count=y&:origin=viz_share_link",
+          url: "https://public.tableau.com/views/EntryLevelGoogleProductManagervsGoldmanSachsAnalystinNYCUSD/Sheet4?:display_count=y&:origin=viz_share_link",
         },
         "Consulting": {
           name: "Consulting",
           label: "McKinsey, Bain, BCG, and more",
-          vizUrl: "https://public.tableau.com/views/WhatSchoolsDoCompaniesHireFromOrganizedbyCompanySchoolsNumbers/Sheet2?:display_count=y&:origin=viz_share_link",
+          url: "https://public.tableau.com/views/WhatSchoolsDoCompaniesHireFromOrganizedbyCompanySchoolsNumbers/Sheet2?:display_count=y&:origin=viz_share_link",
         },
       }
     };
@@ -62,7 +62,7 @@ export default class EbookPremiumTabs extends Component {
               {Object.keys(industryConfig).map( industry => (
                   <TabPane tab={industry} key={industry}>
                     <React.Fragment>
-                      <TableauViz url={industryConfig[industry].vizUrl} />
+                      <TableauViz url={industryConfig[industry].url} />
                     </React.Fragment>
                   </TabPane>
               ))}
