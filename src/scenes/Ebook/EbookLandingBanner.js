b54d6f87ea55a8cb9c2c7ba126070da08b87a623
ademidun <tomiademidun@gmail.com>
2020-04-28 10:06:04 -0400

    preload pdf preview iframe

--- src/scenes/Ebook/EbookLandingBanner.js
+++ src/scenes/Ebook/EbookLandingBanner.js
@@ -105,7 +105,9 @@ class EbookLandingBanner extends Component {
             style={{ width: "100%", height: "75vh" }}
           />
         </Modal>
-        <link rel="preload" href="https://example.com/widget.html" as="document" />
+        <link rel="preload"
+              href="https://atila-schools-and-jobs-guide.s3.amazonaws.com/atila-ebook-online-preview.pdf"
+              as="document" />
       </div>
     );
   }
