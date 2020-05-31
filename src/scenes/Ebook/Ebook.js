6f152dffb79c44d0753ee50139cb5c114f1e7dc3
ademidun <tomiademidun@gmail.com>
2020-05-30 20:32:13 -0400

    move audience props to constructior() for Ebook

--- src/scenes/Ebook/Ebook.js
+++ src/scenes/Ebook/Ebook.js
@@ -12,57 +12,70 @@ import {BackTop} from "antd";
 import {unSlugify} from "../../services/utils";
 import {EBOOK_AUDIENCE_IMAGES} from "../../models/Constants";
 
-function Ebook({location: { search }}) {
+class Ebook extends React.Component {
 
-  const params = new URLSearchParams(search);
-  const audience = unSlugify(params.get('audience') || '1');
+    constructor(props){
+        super(props);
+        const {location: { search }} = props;
+        const params = new URLSearchParams(search);
+        const audience = unSlugify(params.get('audience') || '1');
+        console.log({search, audience});
 
+        this.state = {
+            audience,
+        }
 
-  console.log({search, audience});
-
-
-  const seoContent = {
-    title:
-        "Atila Schools and Jobs Guide | The Best Canadian Universities for the Best Jobs",
-    description:
-        "A guide to The best Canadian Universities for getting jobs at Goldman Sachs, Google, McKinsey, Pfizer and more.",
-    image: EBOOK_AUDIENCE_IMAGES[audience].seoImage,
-    slug: "/schools",
-  };
+    }
 
 
-  return (
-    <React.Fragment>
-      <div>
-        <HelmetSeo content={seoContent} />
-        <BackTop />
-        <EbookLandingBanner audience={audience} />
-        <hr />
-        <EmailSignUp />
-        <hr />
-        <EbookPreview />
-        <hr />
-        <TableauGraphsEmbed />
-        <hr />
-        <PremiumDescription />
-        <hr />
-        <EbookInterviews />
-        <hr />
-        <Team
-          showArray={["Tomiwa", "Melissa", "Isaac", "Elaine", "Aaron", "Dev", "Hadi", "Grace", "Emily"]}
-          showLinkedin={false}
-          showSeo={false}
-        />
-        <hr />
-        <EbookVideoEmbed />
-        <hr />
-        <EbookLandingBanner />
-        <hr />
-        <EmailSignUp />
-        <hr />
-      </div>
-    </React.Fragment>
-  );
+    render() {
+
+        const { audience } = this.state;
+        console.log({audience});
+
+        const seoContent = {
+            title:
+                "Atila Schools and Jobs Guide | The Best Canadian Universities for the Best Jobs",
+            description:
+                "A guide to The best Canadian Universities for getting jobs at Goldman Sachs, Google, McKinsey, Pfizer and more.",
+            image: EBOOK_AUDIENCE_IMAGES[audience].seoImage,
+            slug: "/schools",
+        };
+
+
+        return (
+            <React.Fragment>
+                <div>
+                    <HelmetSeo content={seoContent} />
+                    <BackTop />
+                    <EbookLandingBanner audience={audience} />
+                    <hr />
+                    <EmailSignUp />
+                    <hr />
+                    <EbookPreview />
+                    <hr />
+                    <TableauGraphsEmbed />
+                    <hr />
+                    <PremiumDescription />
+                    <hr />
+                    <EbookInterviews />
+                    <hr />
+                    <Team
+                        showArray={["Tomiwa", "Melissa", "Isaac", "Elaine", "Aaron", "Dev", "Hadi", "Grace", "Emily"]}
+                        showLinkedin={false}
+                        showSeo={false}
+                    />
+                    <hr />
+                    <EbookVideoEmbed />
+                    <hr />
+                    <EbookLandingBanner />
+                    <hr />
+                    <EmailSignUp />
+                    <hr />
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Ebook;
