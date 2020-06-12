fd7cf4f81903e573f777b7bed763871df08ec28c
ademidun <tomiademidun@gmail.com>
2020-06-12 09:40:03 -0400

    add follow atilatech on instagram back to navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -159,6 +159,12 @@ function Footer({location, history}) {
                   </div>
                 </div>
               </Col>
+              <Col md={{ span: 6, offset: 3 }} sm={{ span: 12, offset: 6 }} xs={{ span: 24 }}>
+                <a  target="_blank"
+                    rel="noopener noreferrer" href="https://www.instagram.com/atilatech/">
+                  Follow @atilatech on Instagram <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </Col>
             </Row>
           </div>
           <Row className="bottom-bar">
