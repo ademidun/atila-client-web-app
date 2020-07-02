6c75dbdbc792541be6c7f251dba417bf12c14029
ademidun <tomiademidun@gmail.com>
2020-07-02 08:47:53 -0400

    added boilerplate text and Ant design collapse component to FAQ

--- src/scenes/Ebook/EbookFAQ.js
+++ src/scenes/Ebook/EbookFAQ.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { Collapse } from 'antd';
+
+const { Panel } = Collapse;
+
+function callback(key) {
+    console.log(key);
+}
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
 
 function EbookFAQ() {
 
 
     return (
-        <div>
-            <h1>Ebook FAQ</h1>
+        <div className="container">
+            <h1>Frequently Asked Questions (FAQ)</h1>
+            <Collapse defaultActiveKey={['1']} onChange={callback}>
+                <Panel header="This is panel header 1" key="1">
+                    <p>{text}</p>
+                </Panel>
+                <Panel header="This is panel header 2" key="2">
+                    <p>{text}</p>
+                </Panel>
+                <Panel header="This is panel header 3" key="3" disabled>
+                    <p>{text}</p>
+                </Panel>
+            </Collapse>
         </div>
     );
 }
