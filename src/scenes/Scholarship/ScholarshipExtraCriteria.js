edd675019decc061b8c501d2cae6d17841c1f625
Hadi <hadinawar@hotmail.com>
2020-09-08 21:57:00 -0400

    fixed improper capitalization

--- src/scenes/Scholarship/ScholarshipExtraCriteria.js
+++ src/scenes/Scholarship/ScholarshipExtraCriteria.js
@@ -44,7 +44,7 @@ function ScholarshipExtraCriteria({scholarship, loggedInUserProfile, viewAsUserP
                     {
                         userProfile && userProfile[criteria] && userProfile[criteria].length===0 &&
                         <p>
-                            Don't qualify for scholarships of these {prettifyKeys(criteria)}?&nbsp;
+                            Don't qualify for scholarships of these {prettifyKeys(criteria).toLowerCase()}?&nbsp;
                             <Link to={`/profile/${userProfile.username}/edit`}>
                                 Edit Profile</Link> to see correct scholarships
                         </p>
@@ -68,7 +68,7 @@ function ScholarshipExtraCriteria({scholarship, loggedInUserProfile, viewAsUserP
                                 userProfile[locationType].length===0 &&
                                 index===0 &&
                                 <p>
-                                    Don't qualify for scholarships of these {prettifyKeys(locationType)}?&nbsp;
+                                    Don't qualify for scholarships of these {prettifyKeys(locationType).toLowerCase()}?&nbsp;
                                     <Link to={`/profile/${userProfile.username}/edit`}>
                                         Edit Profile</Link> to see correct scholarships
                                 </p>
