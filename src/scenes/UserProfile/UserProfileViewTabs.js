d012a58638e445a1a92286cef47aa8d3dd596177
tomiwa <tomiademidun@gmail.com>
2020-10-03 07:14:48 -0400

    removed 'My Applications' tab, that should go in a seperate PR

--- src/scenes/UserProfile/UserProfileViewTabs.js
+++ src/scenes/UserProfile/UserProfileViewTabs.js
@@ -63,11 +63,6 @@ class UserProfileViewTabs extends React.Component {
             <div className='mt-3'>
                 <Tabs defaultActiveKey={defaultActiveKey} transition={false} id="UserProfileViewTabs">
                     {isProfileEditable &&
-                    <Tab eventKey='applications' title='My Applications'>
-                        <UserProfileViewSavedScholarships />
-                    </Tab>
-                    }
-                    {isProfileEditable &&
                     <Tab eventKey='scholarships' title='Saved Scholarships'>
                         <UserProfileViewSavedScholarships />
                     </Tab>
