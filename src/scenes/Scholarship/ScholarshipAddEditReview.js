7336916354060100509c2a6ca4995072a3914989
Hadi <hadinawar@hotmail.com>
2020-10-10 16:29:16 -0400

    disabled fund scholarship button if scholarship is funded, and tells the user the scholarship is funded [skip ci]

--- src/scenes/Scholarship/ScholarshipAddEditReview.js
+++ src/scenes/Scholarship/ScholarshipAddEditReview.js
@@ -29,12 +29,18 @@ export function ScholarshipAddEditReview ({scholarship}){
                     <br/>
                     Total Amount: {formatCurrency(totalScholarshipPlusFees)}
                 </p>
+                {!scholarship.is_funded &&
                 <Button type="primary">
                     <Link to={`/payment/send/?scholarship=${scholarship.id}`}>
                         Fund Scholarship
                     </Link>
                 </Button>
-
+                }
+                {scholarship.is_funded &&
+                <p style={{fontSize: "1.5rem", lineHeight: "45px", color: 'green'}}>
+                    Your scholarship has been funded!
+                </p>
+                }
             </div>
         </div>
     )
