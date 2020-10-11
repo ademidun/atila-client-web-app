a20505393a4e846e6aee42b6d21ac6771eb4b126
Hadi <hadinawar@hotmail.com>
2020-10-11 17:53:01 -0400

    ScholarshipViewQuestions now gets the scholarship using API

--- src/scenes/Scholarship/ScholarshipViewQuestions.js
+++ src/scenes/Scholarship/ScholarshipViewQuestions.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import {connect} from "react-redux";
+import ScholarshipsAPI from "../../services/ScholarshipsAPI";
+import Loading from "../../components/Loading";
 
 class ScholarshipViewQuestions extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            scholarship: null,
+            isLoadingScholarship: true
+        }
+    }
+
+    componentDidMount() {
+        const { match : { params : { slug }}} = this.props;
+
+        ScholarshipsAPI
+            .getSlug(slug)
+            .then(res => {
+                const scholarship = res.data;
+                this.setState({ scholarship });
+            })
+            .catch(err => {
+                console.log({err})
+            })
+            .finally(() => {
+                this.setState({isLoadingScholarship: false});
+            })
     }
 
     render () {
+        const { isLoadingScholarship } = this.state
+
+        if (isLoadingScholarship) {
+            return (<Loading title={`Loading Form`} className='mt-3' />)
+        }
+
         return (
             <div className="container mt-5">
                 <h1>Testing</h1>
