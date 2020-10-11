915a50d03c8213fd677794e9debba5895e9b6d7b
ademidun <tomiademidun@gmail.com>
2020-10-11 09:09:57 -0400

    changed default pricing from 5% to 9%

--- src/scenes/Payment/Pricing.js
+++ src/scenes/Payment/Pricing.js
@@ -2,12 +2,18 @@ import React from "react";
 import './Pricing.scss';
 import {Link} from "react-router-dom";
 import HelmetSeo, {defaultSeoContent} from "../../components/HelmetSeo";
+import {ATILA_SCHOLARSHIP_FEE} from "../../models/Constants";
+
+
+
 
 function Pricing() {
+
+    const ATILA_SCHOLARSHIP_FEE_AS_INTEGER = Number.parseInt(ATILA_SCHOLARSHIP_FEE * 100);
     // todo generate sitemap dynamically
     const seoContent = {
-        title: 'Atila Pricing - Free for Students. 5% for Scholarship sponsors',
-        description: 'Atila is free for students and Scholarship sponsors pay 5% of the scholarship amount.',
+        title: `Atila Pricing - Free for Students. ${ATILA_SCHOLARSHIP_FEE_AS_INTEGER}% for Scholarship sponsors.`,
+        description: `Atila is free for students and Scholarship sponsors pay ${ATILA_SCHOLARSHIP_FEE_AS_INTEGER}% of the scholarship value.`,
         image: defaultSeoContent.image,
         slug: '/premium'
     };
@@ -21,30 +27,34 @@ function Pricing() {
                 <div className="background">
                     <div className="container">
                         <h1>Pricing</h1>
+                        <h5 className="text-white text-center">
+                            Atila is completely free for students. Sponsors pay {ATILA_SCHOLARSHIP_FEE_AS_INTEGER}% of the total scholarship value.
+                        </h5>
                         <div className="panel pricing-table">
 
                             <div className="pricing-plan">
                                 <img src="https://s22.postimg.cc/8mv5gn7w1/paper-plane.png
                                 " alt="" className="pricing-img" />
+                                <div className="pricing-plan-text">
                                 <h2 className="pricing-header">Student</h2>
                                 <span className="pricing-price">Free</span>
+                                </div>
                                 <Link to="/register" className="pricing-button">
                                     Sign up
                                 </Link>
-                                <ul className="pricing-features">
-                                </ul>
                             </div>
 
                             <div className="pricing-plan">
-                                <br/>
                                 <img src="https://s21.postimg.cc/tpm0cge4n/space-ship.png" alt="" className="pricing-img" />
-                                <h3 className="pricing-header-last"> Scholarship Sponsors</h3>
-                                <span className="pricing-price">5%</span>
+                                <div className="pricing-plan-text">
+                                <h2 className="pricing-header-last"> Scholarship Sponsors</h2>
+                                <span className="pricing-price">{ATILA_SCHOLARSHIP_FEE_AS_INTEGER}%</span>
                                 <li className="pricing-features-item">
                                     of total scholarship value
                                 </li>
-                                <Link to="/contact" className="pricing-button is-featured">
-                                    Learn More
+                                </div>
+                                <Link to="/start" className="pricing-button">
+                                    Start a Scholarship
                                 </Link>
                             </div>
 
