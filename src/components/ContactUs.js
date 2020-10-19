01ac4536fd1382189265e30fea1bf4f6bb3cc51b
ademidun <tomiademidun@gmail.com>
2020-10-19 07:38:53 -0400

    Fixed compiler warnings
    Compiled with warnings.
    
    ./src/components/ContactUs.js
      Line 102:76:  Using target="_blank" without rel="noopener noreferrer" is a security risk: see https://mathiasbynens.github.io/rel-noopener  react/jsx-no-target-blank
    
    Search for the keywords to learn more about each warning.

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -99,7 +99,7 @@ class ContactUs extends  React.Component{
                     in the bottom right of your screen or send us an email at {' '}
                     <a href="mailto:info@atila.ca" target="_blank" rel="noopener noreferrer">
                         info@atila.ca </a>
-                    or ask us on <a href="https://www.reddit.com/r/atila/" target="_blank">reddit</a>
+                    or ask us on <a href="https://www.reddit.com/r/atila/" target="_blank" rel="noopener noreferrer" >reddit (r/atila)</a>
                 </p>
                 <form className="row p-3 form-group" onSubmit={this.submitContact}>
                     <input placeholder="Full Name"
