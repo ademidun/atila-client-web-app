ceda6fe41e9e0d815031e39c733099db31a9337d
ademidun <tomiademidun@gmail.com>
2020-10-30 09:09:37 -0400

    add youtube embed for 'How to Apply For Scholarships on Atila'

--- src/scenes/LandingPage/LandingPageLiveDemo.js
+++ src/scenes/LandingPage/LandingPageLiveDemo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function LandingPageLiveDemo() {
+function LandingPageLiveDemo({youtubeVideoId="MLnBK2ehXdQ", title="How To Add A Scholarship To Atila"}) {
 
     return (
         <div className="container center-block">
             <div className="videowrapper text-center">
-                <iframe title="How To Add A Scholarship To Atila"
-                        src="//www.youtube.com/embed/MLnBK2ehXdQ?cc_load_policy=1&cc_lang_pref=en"
+                <iframe title={title}
+                        src={`//www.youtube.com/embed/${youtubeVideoId}?cc_load_policy=1&cc_lang_pref=en`}
                         width="560" height="314" style={{maxWidth: '80%'}}>
                 </iframe>
             </div>
