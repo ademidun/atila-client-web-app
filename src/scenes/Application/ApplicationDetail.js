e62cf07481f771643e6cb7771b2beb3a6c71e140
Tomiwa Ademidun <tomiademidun@gmail.com>
2020-12-07 08:49:22 -0500

    moved some Application utility functions into ApplicationUtils

--- src/scenes/Application/ApplicationDetail.js
+++ src/scenes/Application/ApplicationDetail.js
@@ -10,9 +10,7 @@ import {connect} from "react-redux";
 import moment from "moment";
 import ApplicationsAPI from "../../services/ApplicationsAPI";
 import Loading from "../../components/Loading";
-import {SCHOLARSHIP_QUESTIONS_TYPES_TO_FORM_TYPES} from "../../models/Scholarship";
-import {userProfileFormConfig} from "../../models/UserProfile";
-import {scholarshipUserProfileSharedFormConfigs, toastNotify} from "../../models/Utils";
+import {toastNotify} from "../../models/Utils";
 import FormDynamic from "../../components/Form/FormDynamic";
 import {Link} from "react-router-dom";
 import {Button, Popconfirm} from "antd";
@@ -21,6 +19,11 @@ import Register from "../../components/Register";
 import HelmetSeo, {defaultSeoContent} from "../../components/HelmetSeo";
 import ScholarshipsAPI from "../../services/ScholarshipsAPI";
 import SecurityQuestionAndAnswer from "./SecurityQuestionAndAnswer";
+import {
+    addQuestionDetailToApplicationResponses,
+    transformProfileQuestionsToApplicationForm,
+    transformScholarshipQuestionsToApplicationForm
+} from "./ApplicationUtils";
 // import SecurityQuestionAndAnswer from "./SecurityQuestionAndAnswer";
 
 let autoSaveTimeoutId;
@@ -600,106 +603,6 @@ class ApplicationDetail extends  React.Component{
             </>
         );
     }
-}
-
-/**
- * Transform array of questions of the form:
- * {
-  "key": "why-do-you-deserve-this-scholarship",
-  "question": "Why do you deserve this scholarship?",
-  "type": "long_answer"
-} into format:
-
- {
-        keyName: "why-do-you-deserve-this-scholarship",
-        placeholder: "Why do you deserve this scholarship?",
-        label: "Why do you deserve this scholarship?"
-        type: 'html_editor'
-    }
- * @param questions
- */
-export function transformScholarshipQuestionsToApplicationForm(questions) {
-
-    return questions.map(question => (
-        {
-            keyName: question.key,
-            placeholder: question.question,
-            label: question.question,
-            type: SCHOLARSHIP_QUESTIONS_TYPES_TO_FORM_TYPES[question.type],
-        }
-    ));
-}
-
-/***
- * Takes an array of questions in the following format and return all the elements that match the UserProfile questions.
- * @param scholarshipProfileQuestions
- * @returns {*}
- */
-export function transformProfileQuestionsToApplicationForm(scholarshipProfileQuestions) {
-
-    const formQuestions = [];
-    const allProfileQuestions = [...userProfileFormConfig, ...scholarshipUserProfileSharedFormConfigs];
-
-    scholarshipProfileQuestions.forEach(scholarshipProfileQuestion => {
-        const positionInAllProfileQuestions =  allProfileQuestions.findIndex((item) => scholarshipProfileQuestion.key === item.keyName)
-
-        if (positionInAllProfileQuestions > -1) {
-            formQuestions.push(allProfileQuestions[positionInAllProfileQuestions]);
-        }
-
-    });
-
-    return formQuestions
-}
-
-/**
- * For an application that has application.scholarship_responses
- * in the form {"why-do-you-deserve-this-scholarship": "I am awesome."},
- * for each response, add info about the question:
- * {
-  "key": "why-do-you-deserve-this-scholarship",
-  "question": "Why do you deserve this scholarship?",
-  "type": "long_answer"
-  "response": "I am awesome."
-}
- * @param application
- * @param scholarship
- * @returns {*}
- */
-function addQuestionDetailToApplicationResponses(application, scholarship) {
-
-
-    const scholarshipResponses = {};
-    const userProfileResponses = {};
-    Object.keys(application.scholarship_responses).forEach( responseKey => {
-        const question = scholarship.specific_questions.find(question => {
-            return question.key === responseKey
-        });
-        if (question) {
-            scholarshipResponses[responseKey] = {
-                ...question,
-                "response": application.scholarship_responses[responseKey]
-            }
-        }
-    });
-
-    Object.keys(application.user_profile_responses).forEach( responseKey => {
-        const question = scholarship.user_profile_questions.find(question => {
-            return question.key === responseKey
-        });
-                if (question) {
-            userProfileResponses[responseKey] = {
-                ...question,
-                response: application.user_profile_responses[responseKey]
-            }
-        }
-    });
-
-    return {
-        scholarship_responses: scholarshipResponses,
-        user_profile_responses: userProfileResponses,
-    };
-
 }
 
 
