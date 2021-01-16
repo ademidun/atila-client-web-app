106df70a8e1e576899e6489ffa8ae01c11a4d7d0
Tomiwa Ademidun <tomiademidun@gmail.com>
2021-01-16 17:19:50 -0500

    replace landing page image for SEO
    to use new image with the big cheque

--- src/components/HelmetSeo.js
+++ src/components/HelmetSeo.js
@@ -6,7 +6,7 @@ import $ from 'jquery';
 export const defaultSeoContent = {
     title: 'Atila - The easiest way to start and get scholarships.',
     description: 'The easiest way to start and get scholarships.',
-    image: 'https://i.imgur.com/nib7LyD.png',
+    image: 'https://i.imgur.com/Cm61X7Y.png',
     slug: ''
 };
 
