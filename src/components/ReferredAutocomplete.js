489df8b53fbbeadac3061ddaa6526ff78b7abc58
hadialhakeem <hadihakeem2@gmail.com>
2021-01-22 17:03:14 -0500

    added space

--- src/components/ReferredAutocomplete.js
+++ src/components/ReferredAutocomplete.js
@@ -14,6 +14,7 @@ class ReferredAutocomplete extends React.Component {
             referredBy: ""
         }
     }
+
     updateReferredByField = (newReferredByField) => {
         const { onFieldUpdate } = this.props;
 
