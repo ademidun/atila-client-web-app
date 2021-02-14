de62a0c74494e5e9bbf5d302243b9bf207c187ef
Eric Wang <ericwang451@gmail.com>
2021-02-14 00:36:30 -0800

    added laurentian, langara, Dalhousie, Ryerson, and Georgetown DSB to social proof

--- src/scenes/LandingPage/SocialProof.js
+++ src/scenes/LandingPage/SocialProof.js
@@ -8,7 +8,72 @@ const schoolImages = [
     {
         "url": "https://imgur.com/6kJAH06.jpg",
         "alt": "Ivey Business School",
-    }
+    },
+    {
+        "url": "https://i.imgur.com/BSIjvEC.jpg",
+        "alt": "McGill University",
+    },
+    {
+        "url": "https://i.imgur.com/zOQZsdL.jpg",
+        "alt": "University of British Columbia",
+    },
+    {
+        "url": "https://i.imgur.com/XPjzuEl.jpg",
+        "alt": "University of Alberta",
+    },
+    {
+        "url": "https://i.imgur.com/eHdi4cu.jpg",
+        "alt": "Simon Fraser University",
+    },
+    {
+        "url": "https://i.imgur.com/aPk1hmk.jpg",
+        "alt": "Humber College",
+    },
+    {
+        "url": "https://i.imgur.com/YLUk42o.jpg",
+        "alt": "Carleton University",
+    },
+    {
+        "url": "https://i.imgur.com/erf5H8M.jpg",
+        "alt": "Ryerson University",
+    },
+    {
+        "url": "https://i.imgur.com/IWerHJh.jpg",
+        "alt": "Dalhousie University",
+    },
+    {
+        "url": "https://i.imgur.com/w2ztHKJ.png",
+        "alt": "Langara College",
+    },
+    {
+        "url": "https://i.imgur.com/Z4IkK8h.png",
+        "alt": "Laurentian University",
+    },
+    {
+        "url": "https://i.imgur.com/oJu53qu.jpg",
+        "alt": "Toronto District School Board",
+    },
+    {
+        "url": "https://i.imgur.com/MCacdYu.jpg",
+        "alt": "Waterloo Region District School Board",
+    },
+    {
+        "url": "https://i.imgur.com/CBbAmV0.jpg",
+        "alt": "Durham District School Board",
+    },
+    {
+        "url": "https://i.imgur.com/bwUy1WL.jpg",
+        "alt": "Waterloo Catholic District School Board",
+    },
+    {
+        "url": "https://i.imgur.com/XghA3ft.jpg",
+        "alt": "Halton District School Board",
+    },
+    {
+        "url": "https://i.imgur.com/15trTBH.jpg",
+        "alt": "Georgetown District School Board",
+    },
+
 ]
 
 export function SocialProof() {
@@ -23,23 +88,6 @@ export function SocialProof() {
                     {schoolImages.map (schoolImage => (
                     <img src={schoolImage.url} width="150" height="auto" alt={schoolImage.alt}/>
                     ))}
-                    <img src="https://imgur.com/BSIjvEC.jpg" width="170" height="70" alt="McGill University"></img>
-                    <img src="https://imgur.com/zOQZsdL.jpg" width="150" height="75" alt="University of British Columbia"></img>
-                    <img src="https://imgur.com/XPjzuEl.jpg" width="155" height="50" alt="University of Alberta"></img>
-                    <img src="https://imgur.com/eHdi4cu.jpg" width="175" height="80" alt="Simon Fraser University"></img>
-                    <img src="https://imgur.com/aPk1hmk.jpg" width="120" height="60" alt="Humber College"></img>
-                    <img src="https://imgur.com/YLUk42o.jpg" width="120" height="70" alt="Carleton University"></img>
-                    <img src="https://i.imgur.com/w2ztHKJ.png" width="150" height="auto" alt="Langara College"></img>
-                </div>
-                <div>
-                    <img src="https://imgur.com/oJu53qu.jpg" width="150" height="120" alt="Toronto District School Board"></img>
-                    <img src="https://imgur.com/MCacdYu.jpg" width="180" height="85"
-                         alt="Waterloo Region District School Board"></img>
-                    <img src="https://imgur.com/CBbAmV0.jpg" width="120" height="130" alt="Durham District School Board"></img>
-                    <img src="https://imgur.com/bwUy1WL.jpg" width="190" height="120"
-                         alt="Waterloo Catholic District School Board"></img>
-                    <img src="https://imgur.com/XghA3ft.jpg" width="130" height="120"
-                         alt="Halton District School Board"></img>
                 </div>
             </div>
         </div>
