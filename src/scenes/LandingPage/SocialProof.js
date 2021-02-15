0a2d413466702dcb2c398bfffb24332f801d3ae6
Eric Wang <ericwang451@gmail.com>
2021-02-14 22:02:14 -0800

    customized width and height for every school

--- src/scenes/LandingPage/SocialProof.js
+++ src/scenes/LandingPage/SocialProof.js
@@ -4,74 +4,104 @@ const schoolImages = [
     {
         "url": "https://imgur.com/VXhmyOx.jpg",
         "alt": "University of Toronto",
+        "width":"120",
+        "height":"90",
     },
     {
         "url": "https://imgur.com/6kJAH06.jpg",
         "alt": "Ivey Business School",
+        "width":"120",
+        "height":"75",
     },
     {
         "url": "https://i.imgur.com/BSIjvEC.jpg",
         "alt": "McGill University",
+        "width":"140",
+        "height":"60",
     },
     {
         "url": "https://i.imgur.com/zOQZsdL.jpg",
         "alt": "University of British Columbia",
+        "width":"150",
+        "height":"75",
     },
     {
         "url": "https://i.imgur.com/XPjzuEl.jpg",
         "alt": "University of Alberta",
+        "width":"155",
+        "height":"50",
     },
     {
         "url": "https://i.imgur.com/eHdi4cu.jpg",
         "alt": "Simon Fraser University",
+        "width":"175",
+        "height":"80",
     },
     {
         "url": "https://i.imgur.com/aPk1hmk.jpg",
         "alt": "Humber College",
+        "width":"110",
+        "height":"65",
     },
     {
         "url": "https://i.imgur.com/YLUk42o.jpg",
         "alt": "Carleton University",
+        "width":"120",
+        "height":"70",
     },
     {
         "url": "https://i.imgur.com/erf5H8M.jpg",
         "alt": "Ryerson University",
+        "width":"120",
+        "height":"75",
     },
     {
         "url": "https://i.imgur.com/IWerHJh.jpg",
         "alt": "Dalhousie University",
+        "width":"140",
+        "height":"50",
     },
     {
         "url": "https://i.imgur.com/w2ztHKJ.png",
         "alt": "Langara College",
+        "width":"140",
+        "height":"150",
     },
     {
         "url": "https://i.imgur.com/Z4IkK8h.png",
         "alt": "Laurentian University",
+        "width":"210",
+        "height":"50",
     },
     {
         "url": "https://i.imgur.com/oJu53qu.jpg",
         "alt": "Toronto District School Board",
+        "width":"130",
+        "height":"100",
     },
     {
         "url": "https://i.imgur.com/MCacdYu.jpg",
         "alt": "Waterloo Region District School Board",
+        "width":"180",
+        "height":"85",
     },
     {
         "url": "https://i.imgur.com/CBbAmV0.jpg",
         "alt": "Durham District School Board",
+        "width":"90",
+        "height":"100",
     },
     {
         "url": "https://i.imgur.com/bwUy1WL.jpg",
         "alt": "Waterloo Catholic District School Board",
+        "width":"170",
+        "height":"105",
     },
     {
         "url": "https://i.imgur.com/XghA3ft.jpg",
         "alt": "Halton District School Board",
-    },
-    {
-        "url": "https://i.imgur.com/15trTBH.jpg",
-        "alt": "Georgetown District School Board",
+        "width":"100",
+        "height":"85",
     },
 
 ]
@@ -86,7 +116,7 @@ export function SocialProof() {
                 </h3>
                 <div>
                     {schoolImages.map (schoolImage => (
-                    <img src={schoolImage.url} width="150" height="auto" alt={schoolImage.alt}/>
+                    <img src={schoolImage.url} width={schoolImage.width} height={schoolImage.height} alt={schoolImage.alt}/>
                     ))}
                 </div>
             </div>
