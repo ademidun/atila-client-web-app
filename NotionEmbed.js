948d5b1a3d2cbe167365309c12caebd11f24f398
Eric Wang <ericwang451@gmail.com>
2021-05-05 16:32:40 -0400

    created notion embed component for reuse purposes

---
+++ NotionEmbed.js
@@ -0,0 +1,77 @@
+import React from "react";
+import UtilsAPI from "./src/services/UtilsAPI";
+import { NotionRenderer} from "react-notion";
+import {createTableOfContents, scrollToElement} from "./src/services/utils";
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import Loading from "./src/components/Loading";
+
+export class NotionEmbed extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loadingMessage: props.loadingMessage,
+            pageID: props.pageID,
+            loading: false,
+            notionPageData: null,
+        }
+    }
+
+
+    componentDidMount() {
+
+        this.loadNotionPage();
+    }
+
+
+    loadNotionPage() {
+
+        const { location } = this.props;
+
+        this.setState({loading: this.state.loadingMessage});    // set loading message to an input by the constructor
+
+        UtilsAPI.loadNotionContent(this.state.pageID)
+        this.then(res=> {
+
+            this.setState({notionPageData: res.data});
+
+            if (location && location.hash) {
+                createTableOfContents(".embedded-place");
+                // Pause for 300 milliseconds before scrolling to the hash element, without this setTimeout
+                // the element kept scrolling back to the top of the page.
+                setTimeout(() => {
+                    scrollToElement(location.hash);
+                }, 300);
+            }
+        })
+
+        .catch(err => {
+            console.log({err})
+        })
+        .finally( () => {
+            this.setState({loading: false});
+        })
+    }
+
+    //<BackTop/>, classname and seoContent can be added in the parent to which this class is called
+    render() {
+        const { loading, notionPagedata } = this.state;
+
+        return(
+            <div>
+                {loading && <Loading title={loading} /> }
+                <div className="container mt-5">
+                    <div className="card shadow p-3 embedded-place">        //createTableOfContents function will start here
+                        {notionPagedata &&
+                        <div style={{ maxWidth: 768 }}>
+                            <NotionRenderer blockMap={notionPagedata} />
+                        </div>
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
