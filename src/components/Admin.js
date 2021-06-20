fe2c0dc9ad317929b36b748e929ea8b9fb398f3c
Eric Wang <ericwang451@gmail.com>
2021-06-20 11:50:39 -0700

    set up form with all the requisite fields

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
 class Admin extends React.Component {
+
+    constructor(props){
+        super()
+        this.state = {
+            contacts: {}
+        }
+    }
+
+    save_as_contact(){
+
+        console.log("button is clicked")
+    }
    
     render(){
+        
         return (
-            <h1>hello world!</h1> 
+            <div>
+                <h1 style={{padding:"10px"}}>Admin Dashboard</h1> 
+                <form style={{padding: "30px"}} onSubmit={this.save_as_contact}>
+                    <label>
+                        First Name:
+                        <input type="text" name="name" />
+                    </label><br/>
+                    <label>
+                        Last Name:
+                        <input type="text" name="name" />
+                    </label><br/>
+                    <label>
+                        Organization Name:
+                        <input type="text" name="name" />
+                    </label><br/>
+                    <label>
+                        Email:
+                        <input type="text" name="name" />
+                    </label><br/>
+                    <label>
+                        Instagram Username:
+                        <input type="text" name="name" />
+                    </label><br/>
+                    
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+            
         )
     }
 }
