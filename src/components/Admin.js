9812c2d6c99d8d303db1feacc984089de13dc81c
hadialhakeem <hadihakeem2@gmail.com>
2021-06-23 16:30:09 -0400

    added contactAPI file and create method within, used it in Admin.js [skip ci]

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import ContactAPI from "../services/ContactAPI";
 
 class Admin extends React.Component {
 
     constructor(props){
         super()
         this.state = {
-            firstName: '',
-            lastName: '',
+            contact: {},
+            first_name: '',
+            last_name: '',
             email: '',
-            instagramUsername: '',
-            organizationName: '',
+            instagram_username: '',
+            organization_name: '',
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event) {
-        const {name, value} = event.target
+        const { name, value } = event.target;
         this.setState({
              [name]: value
         })
     }
 
-    save_as_contact(){
+    saveAsContact = (event) => {
+        event.preventDefault()
+        
+        const { first_name, last_name, email, instagram_username, organization_name } = this.state;
 
-        const createdContact = this.state
-            
+        const createdContact = {
+            first_name,
+            last_name,
+            email,
+            instagram_username,
+            organization_name
+        }
+
+        console.log(createdContact)
+
+        ContactAPI.create(createdContact)
     }
    
     render(){
@@ -32,29 +46,29 @@ class Admin extends React.Component {
         return (
             <div>
                 <h1 style={{padding:"10px"}}>Admin Dashboard</h1> 
-                <form style={{padding: "30px"}} onSubmit={this.save_as_contact}>
+                <form style={{padding: "30px"}} onSubmit={this.saveAsContact}>
                     <label>
                         First Name:
                         <input 
                             type="text" 
-                            value={this.state.firstName} 
-                            name="firstName" 
+                            value={this.state.first_name} 
+                            name="first_name" 
                             onChange={this.handleChange}/>
                     </label><br/>
                     <label>
                         Last Name:
                         <input 
                             type="text" 
-                            value={this.state.lastName} 
-                            name="lastName" 
+                            value={this.state.last_name} 
+                            name="last_name" 
                             onChange={this.handleChange}/>
                     </label><br/>
                     <label>
                         Organization Name:
                         <input 
                             type="text" 
-                            value={this.state.organizationName} 
-                            name="organizationName"
+                            value={this.state.organization_name} 
+                            name="organization_name"
                             onChange={this.handleChange} />
                     </label><br/>
                     <label>
@@ -69,8 +83,8 @@ class Admin extends React.Component {
                         Instagram Username:
                         <input 
                             type="text" 
-                            value={this.state.instagramUsername} 
-                            name="instagramUsername"
+                            value={this.state.instagram_username} 
+                            name="instagram_username"
                             onChange={this.handleChange} />
                     </label><br/>
                     
