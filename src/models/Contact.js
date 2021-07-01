624b3f519d40a9b589dd151ecfd49ec7b716036d
ademidun <tomiademidun@gmail.com>
2021-07-01 15:09:09 -0400

    change contact form config to be an import
    in case we want to change logic for config at a late date we have more seperation of concerns. Contact.js will hold data information and ContactAddEdit will focus on logic for customizing how the form is rendered

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -1,12 +1,63 @@
-export let DEFAULT_CONTACT = {
+import { prettifyKeys } from '../services/utils';
+import { ALL_DEMOGRAPHICS } from './ConstantsForm';
+
+export const CONTACT_TYPES = ["student_club", "non_profit_company", "for_profit_company",
+"government_organization", "individual"];
+
+export const DEFAULT_CONTACT = {
     organization_name: "",
     instagram_username: "",
     eligible_programs: [],
     heritage:[],
     sports: [],
     industries: [],
-    eligible_schools: []
+    eligible_schools: [],
+    account_type: CONTACT_TYPES[0],
 };
 
-export const CONTACT_TYPES = ["student_club", "non_profit_company", "for_profit_company",
-"government_organization", "individual"];
+export const CONTACT_FORM_CONFIG_PAGE_1 = [
+    {
+        keyName: 'organization_name',
+    },
+    {
+        keyName: 'profile_description',
+        type: 'textarea',
+    },
+    {
+        keyName: 'instagram_username',
+    },
+    {
+        keyName: 'twitter_username',
+    },
+    {
+        keyName: 'profile_pic_url',
+    },
+    {
+        keyName: 'tiktok_username',
+    },
+    {
+        keyName: 'facebook_username',
+    },
+    {
+        keyName: 'linkedin_url',
+    },
+    {
+        keyName: 'website_url',
+    },
+    {
+        keyName: 'account_type',
+        type: 'select',
+        options: CONTACT_TYPES,
+        renderOption: (option) => prettifyKeys(option)
+    }
+];
+
+for (const [demographicKey, demographicOptions] of Object.entries(ALL_DEMOGRAPHICS)) {
+    const inputConfig = {
+        keyName: demographicKey,
+        type: 'autocomplete',
+        suggestions: demographicOptions,
+        skipPrettifyKeys: true,
+    }
+    CONTACT_FORM_CONFIG_PAGE_1.push(inputConfig);
+}
