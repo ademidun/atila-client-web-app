84ab87f6bb687e95a3b78cd796ee9fcbc565e162
ademidun <tomiademidun@gmail.com>
2021-07-10 20:05:37 -0400

    add table to display contacts in a tabular view

---
+++ src/scenes/Contact/ContactsTable.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ContactsAPI from "../../services/ContactsAPI";
+import {toastNotify} from "../../models/Utils";
+import { Table, Spin } from 'antd';
+import DataUtils from '../../services/utils/DataUtils';
+import QueryBuilder from '../../components/Query/QueryBuilder';
+
+
+class ContactsTable extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            contacts: [],
+            loading: null,
+            addContactMode: false,
+        };
+    }
+    /**
+     * TODO: This is the same logic used in ContactsNetwork, is there a way to combine their logic together?
+     * @param {*} queryData 
+     */
+    onUpdateQuery = (queryData) => {
+        this.setState({loading: "Loading clubs..."});
+        ContactsAPI.query(queryData)
+            .then(res => {
+                const { contacts } = res.data;
+                
+                this.setState({ contacts });
+                if (contacts.length === 0) {
+                    toastNotify("No clubs found matching selected query.");
+                }
+            })
+            .catch(err=> {
+                console.log({err});
+                toastNotify("There was problem with your search, please try another one.", "error");
+            })
+            .finally(()=>{
+                this.setState({loading: null})
+            })
+    }
+
+    toggleAddContacts = () => {
+        this.setState({addContactMode: !this.state.addContactMode})
+    }
+
+    render() {
+
+        const { contacts, loading } = this.state;
+
+        let columns = DataUtils.getTableColumnsFromObjects(contacts);
+
+        return (
+            <div className="w-100">
+                <QueryBuilder onUpdateQuery={this.onUpdateQuery} />
+                <Table columns={columns} 
+                       dataSource={contacts} 
+                       rowKey="id"
+                       loading={loading ? { indicator:<Spin />, tip:"Loading Contacts..."} : false}   
+                       style={{ width: '100%', height: '100%' }} />
+            </div>
+        );
+    }
+}
+
+export default ContactsTable;
---
+++ src/services/utils/DataUtils.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class DataUtils {
+    /**
+     * Given a list of objects e.g. contacts, userprofiles, or scholarships.
+     * Configure the table columns for rendering the table view of that object.
+     * @param {*} objects 
+     * @returns 
+     */
+    static getTableColumnsFromObjects = (items) => {
+
+        let columns = [];
+
+        if (items.length > 0) {
+
+            columns = Object.keys(items[0]).map(itemProperty => {
+                
+                const columnSetting = {
+                    title: itemProperty,
+                    dataIndex: itemProperty,
+                    key: itemProperty,
+                };
+                if (itemProperty === "email"){
+                    
+                    columnSetting.render = (email, item) => {
+                        let mailToUrl = email;
+                        // Setting the name in mailto: url might be possible with: https://stackoverflow.com/a/16265466/5405197
+                        if(item.first_name) {
+                            mailToUrl = `mailto:"${item.first_name}${item.last_name? " " + item.last_name: ""}"<${mailToUrl}>`;
+                        } else if (item.organization_name) {
+                            mailToUrl = `mailto:"${item.organization_name}"<${mailToUrl}>`;
+                        }
+                        const mailtoAnchorTag = (<a href={mailToUrl}  target="_blank" rel="noopener noreferrer">
+                        {email}
+                        </a>)
+                        return mailtoAnchorTag;
+                    } 
+                }
+
+                return columnSetting
+            })
+        }
+
+        console.log({columns})
+        return columns;
+    };
+}
+
+export default DataUtils
