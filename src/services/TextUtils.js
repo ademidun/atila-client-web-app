a40082c4660a85919fcdbef103e0e95025682bea
ademidun <tomiademidun@gmail.com>
2021-08-05 22:54:28 -0400

    if str is falsy, return 0

--- src/services/TextUtils.js
+++ src/services/TextUtils.js
@@ -3,7 +3,7 @@ class TextUtils {
     static countWords = (str) => {
         // https://stackoverflow.com/a/18679592/5405197
         //  TODO: The function below doesn't count words on new line as a new word
-        if(str == null) {
+        if(!str) {
             return 0;
         }
         return str.trim().split(/\s+/).length;
