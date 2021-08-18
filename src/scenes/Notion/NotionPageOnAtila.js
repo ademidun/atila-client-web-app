cdbc15bcba1e449aa5ef68d7a68043a1164dcaa9
Eric Wang <ericwang451@gmail.com>
2021-08-18 14:21:06 -0400

    added ability to access pageID from url

--- src/scenes/Notion/NotionPageOnAtila.js
+++ src/scenes/Notion/NotionPageOnAtila.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 class NotionPageOnAtila extends React.Component {
+
     render(){
+    
+        const DEFAULT_PAGE_ID = 'default page id'
+        const urlPath = window.location.pathname.substring(3)
+        
         return(
             <div className="container mt-5">    
-                <p>Sample Notion Page</p>
+                <p>{DEFAULT_PAGE_ID}</p>
+                <p>{urlPath}</p>
             </div>
         )  
     }
