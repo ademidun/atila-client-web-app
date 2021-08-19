0b7de8838d46c01f3c51cb0bd1055d9560fa8964
ademidun <tomiademidun@gmail.com>
2021-08-18 23:09:41 -0400

    change font size of calendar options from 24px to 1.1rem

--- src/components/AddDeadlineToCalendar.js
+++ src/components/AddDeadlineToCalendar.js
@@ -46,7 +46,7 @@ class AddDeadlineToCalendar extends React.Component {
 
         return (
             <div>
-                <Popover trigger={"click"} title={<b>Choose Calendar</b>} content={calendarOptions} placement={"bottom"} overlayStyle={{fontSize: "20px"}}>
+                <Popover trigger={"click"} title={<b>Choose Calendar</b>} content={calendarOptions} placement={"bottom"} overlayStyle={{fontSize: "1.1rem"}}>
                     <Button>Save Deadline To My Calendar</Button>
                 </Popover>
             </div>
