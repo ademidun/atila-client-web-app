de3c4193d537b5b69e9ab2b65fd389b10b89ead6
ademidun <tomiademidun@gmail.com>
2021-08-18 22:54:00 -0400

    use firebase url for the default profile pic url
    safer to use our own image hosting tool for saving something like default pictures for user profiles

--- src/models/UserProfile.js
+++ src/models/UserProfile.js
@@ -105,7 +105,7 @@ export const USER_PROFILE_SECURITY_QUESTIONS = [
 // old default profile
 //https://firebasestorage.googleapis.com/v0/b/atila-7.appspot.com/o/user-profiles%2Fgeneral-data%2Fdefault-profile-pic.png?alt=media&token=455c59f7-3a05-43f1-a79e-89abff1eae57
 
-export const DEFAULT_USER_PROFILE_PICTURE_URL = "https://imgur.com/OI82qoN.jpg";
+export const DEFAULT_USER_PROFILE_PICTURE_URL = "https://firebasestorage.googleapis.com/v0/b/atila-7.appspot.com/o/public%2Fdefault-profile-pic-2021.png?alt=media&token=d6c9d76b-4db2-4e56-89bc-fb51562a6003";
 export function isCompleteUserProfile(userProfile) {
     return (userProfile.post_secondary_school || userProfile.eligible_schools.length !== 0) &&
         (userProfile.major || userProfile.eligible_programs.length !== 0)
