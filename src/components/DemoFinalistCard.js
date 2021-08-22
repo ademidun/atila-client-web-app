4641c1f6740eb898e8be28adbfa3c6eb99fd6a13
bchabi <17bc29@queensu.ca>
2021-08-22 15:36:45 -0400

    Created a demo content card template for the essayists.

---
+++ src/components/DemoFinalistCard.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import './DemoFinalistCard.css'
+
+function DemoCard({ name }, { essaytitle }, { essaycontent }) {
+
+    return (
+
+        <div className='Card'>
+            <div className='upper-container'>
+                <div className='image-container'>
+                    <img src="" alt="" height="100px" width="100px" />
+                </div>
+            </div>
+            <div className='lower-container'>
+                <h3> {name} </h3>
+                <h3> {essaytitle} </h3>
+                <h3> {essaycontent} </h3>
+                <button> Read More</button>
+            </div>
+
+
+        </div>
+
+    )
+
+}
+
+export default DemoCard
