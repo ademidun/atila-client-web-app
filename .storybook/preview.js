5613450b78e41569068f9ac011637f508aba7864
ademidun <tomiademidun@gmail.com>
2021-08-24 22:42:47 -0400

    add ContentCard story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { MemoryRouter } from "react-router";
+import '../src/index.scss';
+import 'antd/dist/antd.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "react-toastify/dist/ReactToastify.css";
+
+  addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
