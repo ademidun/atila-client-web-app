fd2eae4727097fb04433db45e4b3d9bb8c31328d
hadialhakeem <hadihakeem2@gmail.com>
2022-01-30 22:04:27 -0500

    added more currencies

--- src/models/ConstantsPayments.js
+++ src/models/ConstantsPayments.js
@@ -1,24 +1,34 @@
 class Currency {
-    constructor(name, minimum_funding_amount_contribute_new_award, minimum_funding_amount_contribute_scholarship) {
+    constructor(name, code, minimum_funding_amount_contribute_new_award, minimum_funding_amount_contribute_scholarship) {
         this.name = name
+        this.code = code
         this.minimum_funding_amount_contribute_new_award = minimum_funding_amount_contribute_new_award
         this.minimum_funding_amount_contribute_scholarship = minimum_funding_amount_contribute_scholarship
     }
 }
 
 const CAD = new Currency(
-    "CAD", 200, 10
+    "Canadian Dollar", "CAD", 200, 10
 )
 
 const USD = new Currency(
-    "USD", 200, 10
+    "United States Dollar", "USD", 200, 10
+)
+
+const ETH = new Currency(
+    "Ethereum", "ETH", 0.07, 0.02
+)
+
+const BNB = new Currency(
+    "Binance Coin", "BNB", 0.5, 0.09
 )
 
 export const Currencies = {
-    "CAD": CAD, "USD": USD
+    "CAD": CAD, "USD": USD, "ETH": ETH, "BNB": BNB
 }
 Object.freeze(Currencies)
 
+export const CURRENCY_CODES = Object.keys(Currencies).map(key => Currencies[key].code)
 
 export const ATILA_SCHOLARSHIP_FEE = 0.09;
 export const ATILA_SCHOLARSHIP_FEE_TAX = 0.13;
