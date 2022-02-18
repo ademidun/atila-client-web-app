bb34fd55371aee277c72a6b53e7a1504e90d8456
ademidun <tomiademidun@gmail.com>
2022-02-18 16:29:46 -0500

    add separate CTA for demo mode vs prod

--- src/scenes/Crypto/Crypto.tsx
+++ src/scenes/Crypto/Crypto.tsx
@@ -2,11 +2,17 @@ import { Button, Col, Row } from 'antd'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import HelmetSeo, { defaultSeoContent } from '../../components/HelmetSeo';
+import Environment from '../../services/Environment';
 import "./Crypto.scss";
 
 function Crypto() {
 
-    const onboardOptions = [
+
+    const isDemoMode = Environment.name === "demo";
+
+    const cryptoDemoUrl = "https://demo.atila.ca/crypto";
+    const cryptoProdUrl = "https://atila.ca/crypto";
+    let onboardOptions = [
         {
             title: "Apply for a crypto scholarship",
             internal_link: "/scholarship/s/crypto"
@@ -20,8 +26,8 @@ function Crypto() {
             internal_link: "/scholarship/s/crypto"
         },
         {
-            title: "Practice first with test crypto",
-            url: "https://demo.atila.ca/crypto"
+            title: isDemoMode ? "Use real crypto" :  "Practice first with test crypto",
+            url: isDemoMode ? cryptoProdUrl : cryptoDemoUrl,
         },
     ]
 
@@ -42,7 +48,8 @@ function Crypto() {
             </h1>
             <Row gutter={[24, 24]}>
                {onboardOptions.map(option => 
-                <Col xs={24} sm={24} md={6} key={option.title}>
+                // give Col a width of 8 in isDemoMode because there are only 3 buttons and 24/8=3
+                <Col xs={24} sm={24} md={isDemoMode ? 8 : 6} key={option.title}>
                     <Button>
                         {option.url && <a href={option.url} target="_blank" rel="noopener noreferrer">{option.title}</a>}
                         {option.internal_link && <Link to={option.internal_link}>{option.title}</Link>}
@@ -55,7 +62,7 @@ function Crypto() {
         </div>
         <div className="card shadow m-5 p-3">
             <h1>
-                List of available crypto scholarships
+                Crypto scholarships
             </h1>
         </div>
         <div className="card shadow m-5 p-3">
