c969a8d1483fcafb8277bca9ec5cf4f0d12e3df5
ademidun <tomiademidun@gmail.com>
2022-02-19 19:44:55 -0500

    show list of crypto scholarships on /crypto page

--- src/scenes/Crypto/Crypto.tsx
+++ src/scenes/Crypto/Crypto.tsx
@@ -1,14 +1,38 @@
 import { Button, Col, Row } from 'antd'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import HelmetSeo, { defaultSeoContent } from '../../components/HelmetSeo';
+import Loading from '../../components/Loading';
 import NotionPage from '../../components/Notion/NotionPage';
+import { Scholarship } from '../../models/Scholarship.class';
 import Environment from '../../services/Environment';
+import ScholarshipsAPI from '../../services/ScholarshipsAPI';
+import ScholarshipCard from '../Scholarship/ScholarshipCard';
 import "./Crypto.scss";
 
 function Crypto() {
 
+    const [scholarships, setScholarships] = useState<Scholarship[]>([]);
+    const [loadingScholarships, setLoadingScholarships] = useState(false);
 
+    useEffect(() => {
+      loadScholarships();
+    }, [])
+
+    const loadScholarships = () => {
+        setLoadingScholarships(true);
+        ScholarshipsAPI.list('?is_crypto=true')
+        .then(res => {
+            setScholarships(res.data.results);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+        .finally(() => {
+            setLoadingScholarships(false);
+        })
+    }
+    
     const isDemoMode = Environment.name !== "prod";
 
     const cryptoDemoUrl = "https://demo.atila.ca/crypto";
@@ -43,7 +67,7 @@ function Crypto() {
         <h1>{title}</h1>
         <hr />
 
-        <div className="section card shadow">
+        <div className="section card shadow call-to-action">
             <h1>
                 I want to...
             </h1>
@@ -64,6 +88,14 @@ function Crypto() {
             <h1>
                 Crypto scholarships
             </h1>
+            {loadingScholarships && <Loading title="Loading Scholarships" />}
+            <Row gutter={[24, 24]}>
+               {scholarships.map(scholarship => 
+                <Col xs={24} sm={24} md={8} key={scholarship.id}>
+                    <ScholarshipCard scholarship={scholarship} isOneColumn={true}/>
+                </Col>
+            )}
+            </Row>
         </div>
         <div className="section card shadow">
             <h1>
