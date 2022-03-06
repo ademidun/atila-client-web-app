1d52aee38bea73d295efc02fbda842dfefe9bb89
ademidun <tomiademidun@gmail.com>
2022-03-06 10:26:26 -0500

    add support for slugs in  wiki page

--- src/scenes/Wiki/Wiki.tsx
+++ src/scenes/Wiki/Wiki.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import { RouteComponentProps, withRouter } from 'react-router';
+import HelmetSeo, { defaultSeoContent } from '../../components/HelmetSeo';
 import NotionPage from '../../components/Notion/NotionPage';
+import { toTitleCase } from '../../services/utils';
 
 
 interface RouteParams {pageId: string};
 
 function Wiki(props: RouteComponentProps<RouteParams>) {
-
     
-  const { pageId } = props.match.params
+  const { pageId } = props.match.params;
+
+  const pageSlugToPageID: {[key: string]: any} = {
+    careers: '034847710b9e418db750da8c2cee076d'
+  }
+
+  const seoContent = {
+    ...defaultSeoContent,
+    title: `Wiki - ${toTitleCase(pageId)}`,
+  }
+
   return (
-    <div className="card container shadow p-5 m-5">
-        <NotionPage pageId={pageId} />
+    <div className="Wiki card container shadow p-5 mt-5">
+      {pageSlugToPageID[pageId] &&
+      <>
+        <HelmetSeo content={seoContent} />
+        <h1>
+            {toTitleCase(pageId)}
+        </h1>
+      </>
+        
+      }
+        <NotionPage pageId={pageSlugToPageID[pageId] || pageId} />
     </div>
   )
 }
