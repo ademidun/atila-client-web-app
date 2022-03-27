2b16a2603670258b237f456f19dbd2b5cb861855
Grace Liu <graceliu0925@gmail.com>
2022-03-27 11:46:55 -0600

    remove old logic for fetching related items

--- src/components/RelatedItems.js
+++ src/components/RelatedItems.js
@@ -2,11 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ContentCard from "./ContentCard";
-import Loading from "./Loading";
 import {genericItemTransform} from "../services/utils";
-import ScholarshipsAPI from "../services/ScholarshipsAPI";
-import BlogsApi from "../services/BlogsAPI";
-import EssaysApi from "../services/EssaysAPI";
 import Environment from '../services/Environment';
 import { RelatedProducts } from '@algolia/recommend-react';
 import recommend from '@algolia/recommend';
@@ -49,51 +45,8 @@ class RelatedItems extends React.Component {
 
         this.state = {
             relatedItems: [],
-            isLoadingRelatedItems: false,
-            errorLoadingRelatedItems: false,
             id: props.id.toString(),
         }
-    }
-
-    componentDidMount() {
-
-        const { id, itemType } = this.props;
-        this.setState({ isLoadingRelatedItems: true });
-
-        let ContentAPI;
-        switch (itemType) {
-            case "scholarship":
-                ContentAPI = ScholarshipsAPI;
-                break;
-            case "blog":
-                ContentAPI = BlogsApi;
-                break;
-            case "essay":
-                ContentAPI = EssaysApi;
-                break;
-            default:
-                ContentAPI = ScholarshipsAPI;
-                break;
-        }
-
-        let relatedItemsPromise;
-        relatedItemsPromise = ContentAPI
-            .relatedItems(`${id}`)
-            .then(res => {
-                let relatedItems = [];
-                if (res.data.results) {
-                    relatedItems = res.data.results.slice(0,3);
-                }
-                this.setState({ relatedItems });
-            });
-
-        relatedItemsPromise
-            .catch(err => {
-                console.log({ err});
-            })
-            .finally(() => {
-                this.setState({ isLoadingRelatedItems: false });
-            });
     }
 
     getAlogliaIndexName(itemType) {
@@ -114,17 +67,8 @@ class RelatedItems extends React.Component {
 
     render () {
 
-        const { isLoadingRelatedItems, id } = this.state;
+        const { id } = this.state;
         const { className, itemType } = this.props;
-
-        if (isLoadingRelatedItems) {
-            return (
-                <div className={`${className}`}>
-                    <Loading
-                        isLoading={isLoadingRelatedItems}
-                        title={'Loading Related Items..'} />
-                </div>);
-        }
 
         return (
             <div className={`${className}`}>
