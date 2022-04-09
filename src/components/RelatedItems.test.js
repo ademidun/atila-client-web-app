98398b1bccca191578c18f4020b51fe302d4542e
Grace Liu <graceliu0925@gmail.com>
2022-04-09 13:25:22 -0600

    fix tests for redux and hook changes

--- src/components/RelatedItems.test.js
+++ src/components/RelatedItems.test.js
@@ -2,38 +2,43 @@ import React from 'react';
 import {configure, mount, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import "core-js/stable";
-import SearchApi from "../services/SearchAPI";
 import RelatedItems from "./RelatedItems";
 import {ScholarshipEngineering} from "../models/Scholarship";
 import {BlogWhatIsAtila} from "../models/Blog";
 import {MemoryRouter} from "react-router-dom";
 import {EssayIveyApplication} from "../models/Essay";
 import {relatedItems} from "../models/Constants";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { initialReduxState } from "../models/Constants";
 
 configure({ adapter: new Adapter() });
 
-const mockRelatedProducts = `
-<div>
-    <div>
-        <h3>${relatedItems[0].title}</h3>  
-        <h4>${relatedItems[0].slug}</h4>
-        <p>${relatedItems[0].description}</p>  
-    </div>
-    <div>
-        <h3>${relatedItems[1].title}</h3>  
-        <h4>${relatedItems[1].slug}</h4>
-        <p>${relatedItems[1].description}</p>  
-    </div>
-    <div>
-        <h3>${relatedItems[2].title}</h3>  
-        <h4>${relatedItems[2].slug}</h4>
-        <p>${relatedItems[2].description}</p>  
-    </div>
-</div>
-`;
+const mockStore = configureStore();
+const guestUserStore = mockStore(initialReduxState);
+
+const mockUseRelatedProductsResults = {
+    recommendations: [{
+        id: 0,
+        title: relatedItems[0].title,
+        slug: relatedItems[0].slug,
+        description: relatedItems[0].description,
+    }, {
+        id: 1,
+        title: relatedItems[1].title,
+        slug: relatedItems[1].slug,
+        description: relatedItems[1].description,
+    }, {
+        id: 2,
+        title: relatedItems[2].title,
+        slug: relatedItems[2].slug,
+        description: relatedItems[2].description,
+    }],
+    status: 'idle'
+}
 
 jest.mock('@algolia/recommend-react', () => ({
-    RelatedProducts: () => mockRelatedProducts
+    useRelatedProducts: () => { return mockUseRelatedProductsResults; }
 }));
 
 
@@ -41,9 +46,12 @@ describe('<RelatedItems />', () => {
 
     it('renders without crashing', () => {
 
-        const wrapper = shallow(<RelatedItems id={1}
-                                              itemType={'blog'}
-        />);
+        const wrapper = shallow(
+            <MemoryRouter>
+                <Provider store={guestUserStore}>
+                    <RelatedItems id={1} itemType={'blog'}/>
+                </Provider>
+            </MemoryRouter>);
         expect(wrapper.html()).toBeTruthy();
     });
 
@@ -51,7 +59,9 @@ describe('<RelatedItems />', () => {
 
         const wrapper = mount(
             <MemoryRouter>
-                <RelatedItems id={1} itemType={'blog'} />
+                <Provider store={guestUserStore}>
+                    <RelatedItems id={1} itemType={'blog'} />
+                </Provider>
             </MemoryRouter>
         );
 
