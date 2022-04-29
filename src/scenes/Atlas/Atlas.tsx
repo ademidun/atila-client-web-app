7b6a1dc8c471ad761eace217e50a4b4d4d49c073
ademidun <tomiademidun@gmail.com>
2022-04-29 08:57:13 -0400

    add install CTA and explainer Notion page to Atlas landingpage

--- src/scenes/Atlas/Atlas.tsx
+++ src/scenes/Atlas/Atlas.tsx
@@ -1,5 +1,7 @@
+import { Button } from 'antd';
 import React from 'react';
 import HelmetSeo, { defaultSeoContent } from '../../components/HelmetSeo';
+import NotionPage from '../../components/Notion/NotionPage';
 import AtlasPayment from './AtlasPayment/AtlasPayment';
 
 function Atlas() {
@@ -10,16 +12,46 @@ function Atlas() {
         title,
     }
   return (
-    <div className="Crypto container card shadow mt-5 pt-5">
-        <HelmetSeo content={seoContent} />
+    <div className="Crypto container">
+      <div className="card shadow m-5 p-5">
+
+      <HelmetSeo content={seoContent} />
         <h1>Atlas</h1>
-        <h3 className="text-center">A search engine built for crypto.</h3>
+        <h3 className="text-center">A search engine for crypto and web3.</h3>
+
+        <div className="m-3 text-center">
+          <Button type="primary" size="large">
+            <a href="https://chrome.google.com/webstore/detail/atila-atlas/lhjdnmdnomdgjkbefpnehflckklipbak" 
+            target="_blank" 
+            rel="noreferrer">
+              Install Atlas
+            </a>
+            
+          </Button>
+          
+        </div>
+
+        <h1>
+          Get API Credits
+        </h1>
 
         <div className="m-3 text-center">
             <AtlasPayment />
         </div>
+      </div>
+
+
+        <div className="section card shadow m-5 p-5">
+            <h1>
+                What is Atlas?
+            </h1>
+
+            <NotionPage pageId="257f38221c3a4013946217c953bd3db9" showTableOfContents={false} className="p-2" />
+        </div>
         
     </div>
+
+
   )
 }
 
