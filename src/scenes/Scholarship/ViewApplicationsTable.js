9bc69fdabda0a27019567c7974813f40564ab597
hadialhakeem <hadihakeem2@gmail.com>
2022-06-08 18:09:39 -0400

    finished new component

--- src/scenes/Scholarship/ViewApplicationsTable.js
+++ src/scenes/Scholarship/ViewApplicationsTable.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Table} from "antd";
 
 class ViewApplicationsTable extends  React.Component {
     constructor(props) {
@@ -7,9 +8,50 @@ class ViewApplicationsTable extends  React.Component {
     }
 
     render() {
+        const { applications } = this.props;
+
+        if (applications.length === 0) {
+            return (
+                <>
+                    No applications
+                </>
+            )
+        }
+
+
+        let scholarshipResponses = applications.map(application => application.scholarship_responses)
+        let scholarshipKeyToResponses = scholarshipResponses.map(questionDict => {
+            let newDict = {}
+            for (let [questionKey, questionInfo] of Object.entries(questionDict)) {
+                newDict[questionKey] = questionInfo.response
+            }
+            return newDict
+        })
+
+        let columns = []
+        // Get columns from first entry
+        let counter = 1
+        for (let [questionKey, questionInfo] of Object.entries(scholarshipResponses[0])) {
+            const question = questionInfo.question
+
+            columns.push({
+                title: <b>{question}</b>,
+                dataIndex: questionKey,
+                key: counter,
+                render: (response) => {
+                    return <div dangerouslySetInnerHTML={{__html: response}} />
+                }
+            })
+            counter += 1
+        }
+
+
+        console.log(JSON.stringify(scholarshipKeyToResponses))
+        console.log(columns)
+
         return (
             <div>
-                Table here
+                <Table columns={columns} dataSource={scholarshipKeyToResponses} />
             </div>
         );
     }
@@ -18,7 +60,6 @@ class ViewApplicationsTable extends  React.Component {
 ViewApplicationsTable.propTypes = {
     applications: PropTypes.array,
     scholarship: PropTypes.shape({}),
-    loggedInUserProfile: PropTypes.shape({}),
 };
 
 export default ViewApplicationsTable
