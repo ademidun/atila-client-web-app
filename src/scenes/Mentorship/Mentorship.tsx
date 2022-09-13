1d67665380df358c769d3a0754282259b9ba17c3
ademidun <tomiademidun@gmail.com>
2022-09-13 14:44:47 -0400

    create a page to view a mentorship session

--- src/scenes/Mentorship/Mentorship.tsx
+++ src/scenes/Mentorship/Mentorship.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import MentorsList from "./MentorsList";
 import { Route, Switch } from "react-router-dom";
+import MentorshipSessionAddEdit from './MentorshipSession/MentorshipSessionAddEdit';
 
 function Mentorship({ match }: { match: any}) {
     return (
         <Switch>
+            <Route path={`${match.path}/session/new`} component={MentorshipSessionAddEdit} />
+            <Route path={`${match.path}/session/:sessionId`} component={MentorshipSessionAddEdit} />
             <Route
                 exact
                 path={match.path}
---
+++ src/scenes/Mentorship/MentorshipSession/MentorshipSessionAddEdit.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { UserProfile } from '../../../models/UserProfile.class';
+import { Button, Steps } from 'antd';
+
+const { Step } = Steps;
+
+
+const mentorshipSessionSteps = [
+    {
+      title: 'Select',
+      content: ()=> <div>
+        Select Mentor
+      </div>,
+      disabled: () => false,
+    },
+    {
+      title: 'Pay',
+      content: ()=> <div>
+        Pay for Mentor
+      </div>,
+      disabled: () => false,
+    },
+    {
+      title: 'Schedule',
+      content: ()=> <div>
+        Pick a time that works for you
+      </div>,
+      disabled: () => true,
+    },
+    {
+      title: 'Prepare',
+      content: ()=> <div>
+       Fill out an intake form for what you want from the session
+      </div>,
+      disabled: () => true,
+    },
+    {
+      title: 'Attend',
+      content: ()=> <div>
+        Details of your mentorship session
+      </div>,
+      disabled: () => true,
+    },
+];
+
+export interface MentorshipSessionAddEditProps {
+    userProfileLoggedIn?: UserProfile,
+}
+
+export const MentorshipSessionAddEdit = (props: MentorshipSessionAddEditProps) => {
+    const [currentSessionStep, setCurrentSessionStep] = useState(1);
+
+  return (
+    <div className='container card m-3 p-3'>
+      <Steps current={currentSessionStep} onChange={current => setCurrentSessionStep(current)}>
+        {mentorshipSessionSteps.map(item => (
+          <Step key={item.title} title={item.title} disabled={item.disabled()} />
+        ))}
+      </Steps>
+
+      <div>
+        {mentorshipSessionSteps[currentSessionStep].content()}
+      </div>
+
+      <div>
+        {currentSessionStep < mentorshipSessionSteps.length - 1 && (
+          <Button type="primary" className="btn btn-outline-primary float-right col-md-6"
+
+          disabled={mentorshipSessionSteps[currentSessionStep + 1].disabled()}
+            onClick={() => setCurrentSessionStep(currentSessionStep + 1)}>
+            Next
+          </Button>
+        )}
+        {currentSessionStep > 1 && (
+          <Button className="btn btn-outline-primary float-right col-md-6"
+            onClick={() => setCurrentSessionStep(currentSessionStep - 1)} >
+            Previous
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state: any) => {
+    return { userProfileLoggedIn: state.data.user.loggedInUserProfile };
+};
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MentorshipSessionAddEdit)
