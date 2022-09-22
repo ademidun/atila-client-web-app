3887e8359ae7a151bf9af8d8662b013cbe614ee1
ademidun <tomiademidun@gmail.com>
2022-09-22 12:16:06 -0400

    add instructions to mentor profile form

--- src/models/MentorConfig.tsx
+++ src/models/MentorConfig.tsx
@@ -6,14 +6,18 @@ export const mentorProfileFormConfig = [
         type: 'html_editor',
         placeholder: 'Tell mentees about yourself',
         html: () => (<label htmlFor="bio">
-            You bio: tell mentees about yourself
+            You bio: tell mentees about yourself.<br/> Tip: Write this in a seperate document like {' '}
+                    <a href="https://docs.new" target="_blank" rel="noopener noreferrer">
+                     Google docs</a> then copy-paste them back here.
         </label>),
     },
     {
         keyName: 'mentorship_topics',
         type: 'html_editor',
         html: () => (<label htmlFor="mentorship_topics">
-            What topics can you give mentorship about?
+            What topics can you give mentorship about?<br/> Tip: Write this in a seperate document like {' '}
+                    <a href="https://docs.new" target="_blank" rel="noopener noreferrer">
+                     Google docs</a> then copy-paste them back here.
         </label>),
     }
 ];
