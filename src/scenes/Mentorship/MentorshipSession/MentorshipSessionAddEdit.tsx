00baa3cdfefc89f682a7fd2b5f6eae3e566b47f7
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-04-23 10:36:19 +0200

    fix logic for getting session id

--- src/scenes/Mentorship/MentorshipSession/MentorshipSessionAddEdit.tsx
+++ src/scenes/Mentorship/MentorshipSession/MentorshipSessionAddEdit.tsx
@@ -99,7 +99,7 @@ export const MentorshipSessionAddEdit = (props: MentorshipSessionAddEditProps) =
     const handleCalendarEventScheduled = (session: MentorshipSession, eventDetails: any) => {
 
       setNetworkResponse({title: "Saving event details.", type: "loading"});
-      MentorshipAPI.sessionScheduled(id, eventDetails)
+      MentorshipAPI.sessionScheduled(session.id!, eventDetails)
       .then(res => {
         const { data } = res;
         console.log({data});
