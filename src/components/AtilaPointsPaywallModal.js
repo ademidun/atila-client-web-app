c84d10bbac491b94c1cdc47054afff18c3dcd5f4
Tomiwa Ademidun <tomiademidun@gmail.com>
2025-03-24 16:19:06 -0400

    refactor: update AtilaPointsPaywallModal to use React Router v6 hooks and functional component

--- src/components/AtilaPointsPaywallModal.js
+++ src/components/AtilaPointsPaywallModal.js
@@ -1,105 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import {Button, Modal} from 'antd';
-import {connect} from "react-redux";
-import {Link, withRouter} from "react-router-dom";
+import { Button, Modal } from 'antd';
+import { connect } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
 
+function AtilaPointsPaywallModal({ pageViews, userProfile }) {
+    const location = useLocation();
+    const [visible, setVisible] = useState(false);
+    const [viewCount, setViewCount] = useState(null);
 
-class AtilaPointsPaywallModal extends React.Component {
-    constructor(props) {
-        super(props);
+    useEffect(() => {
+        showModalUsingPageViews();
+    }, []);
 
-        const { pageViews } = props;
-        this.state = {
-            visible: false,
-            pageViews,
-            viewCount: null,
-            viewCountType: null,
-        }
-    }
-
-    componentDidMount() {
-        this.showModalUsingPageViews();
-    }
-
-    showModalUsingPageViews = () => {
-        const { pageViews, location : { pathname }, userProfile } = this.props;
-
-        if (pathname === '/blog/atila/what-is-atila') {
-            return
+    const showModalUsingPageViews = () => {
+        if (location.pathname === '/blog/atila/what-is-atila') {
+            return;
         }
 
         if (!userProfile) {
-            this.setState({viewCount: pageViews.guestPageViews, visible: pageViews.guestPageViews % 5 === 0});
+            setViewCount(pageViews.guestPageViews);
+            setVisible(pageViews.guestPageViews % 5 === 0);
         }
     };
 
-    handleOk = e => {
-        this.setState({
-            visible: false,
-        });
+    const handleOk = () => {
+        setVisible(false);
     };
 
-    handleCancel = e => {
-        this.setState({
-            visible: false,
-        });
+    const handleCancel = () => {
+        setVisible(false);
     };
 
-    render() {
-
-        const { userProfile } = this.props;
-        const { viewCount } = this.state;
-
-        if (userProfile) {
-            return null
-        }
-        const guestOrUserProfileContent = (
-                        <span>
-                            <h3>
-                                You have viewed {viewCount} pages
-                            </h3>
-                            <br/>
-                            <h5>
-                                You have not{' '}
-                                <Link to="/register">
-                                    created an account
-                                </Link>
-                            </h5>
-                        </span>);
-
-        const registerCTA = (<Link to="/register">Register for free</Link>);
-        return (
-            <div>
-                <Modal
-                    visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
-                    maskClosable={false}
-                    maskStyle={{background: 'rgba(0,0,0,0.93)'}}
-                    closable={false}
-                    footer={[
-                        <Button key="back"
-                                onClick={this.handleCancel}>
-                            Close
-                        </Button>,
-                        <Button key="submit"
-                                type="primary"
-                                onClick={this.handleOk}>
-                            {registerCTA}
-                        </Button>,
-                    ]}
-                >
-                    <div className="p-3">
-                        {guestOrUserProfileContent}
-                        <br/>
-                        <h4><Link to="/register">Register for free</Link> to keep viewing</h4>
-                    </div>
-
-                </Modal>
-            </div>
-        );
+    if (userProfile) {
+        return null;
     }
+
+    const guestOrUserProfileContent = (
+        <span>
+            <h3>
+                You have viewed {viewCount} pages
+            </h3>
+            <br/>
+            <h5>
+                You have not{' '}
+                <Link to="/register">
+                    created an account
+                </Link>
+            </h5>
+        </span>
+    );
+
+    const registerCTA = (<Link to="/register">Register for free</Link>);
+
+    return (
+        <div>
+            <Modal
+                visible={visible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                maskClosable={false}
+                maskStyle={{background: 'rgba(0,0,0,0.93)'}}
+                closable={false}
+                footer={[
+                    <Button key="back"
+                            onClick={handleCancel}>
+                        Close
+                    </Button>,
+                    <Button key="submit"
+                            type="primary"
+                            onClick={handleOk}>
+                        {registerCTA}
+                    </Button>,
+                ]}
+            >
+                <div className="p-3">
+                    {guestOrUserProfileContent}
+                    <br/>
+                    <h4><Link to="/register">Register for free</Link> to keep viewing</h4>
+                </div>
+            </Modal>
+        </div>
+    );
 }
 
 AtilaPointsPaywallModal.defaultProps = {
@@ -115,4 +97,4 @@ const mapStateToProps = state => {
     return { userProfile: state.data.user.loggedInUserProfile };
 };
 
-export default withRouter(connect(mapStateToProps)(AtilaPointsPaywallModal));
+export default connect(mapStateToProps)(AtilaPointsPaywallModal);
