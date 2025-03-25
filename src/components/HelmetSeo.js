3458e1446dc84816c2b67ffd733340fbd6b7554f
Tomiwa Ademidun <tomiademidun@gmail.com>
2025-03-24 21:08:07 -0400

    create export for defaultSeoContent

--- src/components/HelmetSeo.js
+++ src/components/HelmetSeo.js
@@ -1,3 +1,10 @@
+export const defaultSeoContent = {
+    title: 'Atila - Find and Apply for Scholarships',
+    description: 'Find and apply for scholarships, read student essays and blogs.',
+    image: 'https://i.imgur.com/PMg68If.png',
+    slug: '/'
+};
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
